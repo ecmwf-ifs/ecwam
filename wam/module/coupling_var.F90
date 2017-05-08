cfc0116be523a45f7ca2526b928b4bbaf9d11b7c
Sami Saarinen <Sami.Saarinen@ecmwf.int>
2017-05-08 19:13:00 +0100

    Using explicitly WAM CY43R1

--- wam/module/coupling_var.F90
+++
@@ -1,8 +0,0 @@
-#if defined MODEL_COUPLING_ATM_WAV || defined MODEL_COUPLING_OCN_WAV
-#else
-MODULE COUPLING_VAR 
-  implicit none
-  integer :: WAV_COMM_WORLD 
-  real*8 :: WAV_PresTime, CouplingTime_ATM_WAV, ATM_EXCHANGE, TEST_DIV
-END MODULE COUPLING_VAR
-#endif
