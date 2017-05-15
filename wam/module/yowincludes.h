97dc404985adc4f426c4709c791a2cfa1b446ed5
Michael Sleigh <michael.sleigh@ecmwf.int>
2017-05-15 15:00:55 +0100

    Minor code changes from SPAG experimentation

--- wam/module/yowincludes.h
+++ wam/module/yowincludes.h
@@ -16,9 +16,7 @@
 
 ! gfortran version 
 #ifdef __GFORTRAN__
-#define GCC_VERSION (__GNUC__ * 10000 \
-                    + __GNUC_MINOR__ * 100 \
-                    + __GNUC_PATCHLEVEL__)
+#define GCC_VERSION (__GNUC__ * 10000 + __GNUC_MINOR__ * 100 + __GNUC_PATCHLEVEL__)
 #endif
 
 #define B2MB (1.0/(1024*1024))
