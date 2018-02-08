7c3aabb305363cee8ca62a145f0b4f9487d0bb28
Jean Bidlot <jean.bidlot@ecmwf.int>
2018-02-08 17:05:37 +0000

    out notm work

---
+++ wam/Wam_oper/wamsendnorm.F90
@@ -0,0 +1,60 @@
+      SUBROUTINE WAMSENDNORM(WNORM,ISEND,IRECV)
+
+! ----------------------------------------------------------------------
+
+!**** *WAMSENDNORM* -
+
+
+!*    PURPOSE.
+!     --------
+
+!     SENDS WNORM FROM ISEND TO IRECV
+
+!**   INTERFACE.
+!     ----------
+!     *CALL*  WAMSENDNORM(WNORM,ISEND,IRECV)
+!          *WNORM*     REAL    WAM NORMS 
+!          *ISEND*     INTEGER PE WHERE IT IS DEFINED
+!          *IRECV*     INTEGER PE WHERE IT SHOULD BE WRITTEN OUT 
+
+!     METHOD.
+!     -------
+
+!     EXTERNALS.
+!     ----------
+
+
+!     REFERENCE.
+!     ----------
+
+! ----------------------------------------------------------------------
+      USE PARKIND_WAVE, ONLY : JWIM, JWRB, JWRU
+
+      USE YOWMPP   , ONLY : IRANK
+      USE YOMHOOK   ,ONLY : LHOOK,   DR_HOOK
+      USE MPL_MODULE
+
+! ----------------------------------------------------------------------
+
+      IMPLICIT NONE
+
+      INTEGER(KIND=JWIM), INTENT(IN) :: ISEND, IRECV
+      REAL(KIND=JWRB), INTENT(INOUT) :: WNORM(4)
+
+      REAL(KIND=JWRB) :: ZHOOK_HANDLE
+! ----------------------------------------------------------------------
+      IF (LHOOK) CALL DR_HOOK('WAMSENDNORM',0,ZHOOK_HANDLE)
+
+      IF(ISEND.NE.IRECV) THEN
+        CALL GSTATS(675,0)
+        IF(IRANK.EQ.ISEND) THEN
+          CALL MPL_SEND(WNORM,KDEST=IRECV,CDSTRING='WAMSENDNORM:')
+        ELSEIF(IRANK.EQ.IRECV) THEN
+          CALL MPL_RECV(WNORM,KSOURCE=ISEND,CDSTRING='WAMSENDNORM:')
+        ENDIF
+        CALL GSTATS(675,1)
+      ENDIF
+
+      IF (LHOOK) CALL DR_HOOK('WAMSENDNORM',1,ZHOOK_HANDLE)
+
+      END SUBROUTINE WAMSENDNORM
