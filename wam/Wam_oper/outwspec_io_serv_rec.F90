22d235cfcec09895592aea1e28bbe32974847259
James Hawkes <majh@ecmwf.int>
2018-03-22 08:51:36 +0000

    IFS-279: Added output procedure for wave model spectral fields.

--- wam/Wam_oper/outwspec_io_serv_rec.F90
+++ wam/Wam_oper/outwspec_io_serv_rec.F90
@@ -34,6 +34,8 @@
       USE YOMHOOK  , ONLY : LHOOK, DR_HOOK
       USE YOMIO_SERV, ONLY : IO_SERV
       USE IOFLDDESC_MOD,  ONLY : IOFLDDESC
+      USE GRIB_API_INTERFACE
+      USE GRIB_UTILS_MOD
 
 !-----------------------------------------------------------------------
       IMPLICIT NONE
@@ -41,11 +43,18 @@
       REAL :: ZHOOK_HANDLE
 
       TYPE (IO_SERV),      INTENT (INOUT) :: YDIOS
-      REAL,                INTENT (IN)    :: FIELD (:,:)
+      REAL,                INTENT (IN)    :: FIELD (YDIOS%MODELPAR%YWAM%NGX,YDIOS%MODELPAR%YWAM%NGY)
       TYPE (IOFLDDESC),    INTENT (IN)    :: FLDDESC
       INTEGER (KIND=JPIM), INTENT (INOUT) :: KGRIB_HANDLE
-
-      REAL                    :: ZMISS
+      
+      INTEGER(KIND=JPKSIZE_T)             :: KBYTES
+      INTEGER(KIND=JPIM)                  :: ISIZE, IERR
+      INTEGER, ALLOCATABLE                :: KGRIB_BUFR(:)
+      
+      INTEGER(KIND=JPIM) :: ILEV, IGRIBCD, IDUMMY(1)
+      CHARACTER (LEN=2) :: CLREPR
+      CHARACTER (LEN=3) :: CLTYPE
+      REAL(KIND=JPRB) :: ZDUMMY(1)
 
 !-----------------------------------------------------------------------
 
@@ -53,51 +62,76 @@
       IF (LHOOK) CALL DR_HOOK('OUTWSPEC_IO_SERV_REC',0,ZHOOK_HANDLE)
 #endif
 
-      WRITE(*,*) "OUTWSPEC_IO_SERV_REC"    
-      WRITE(*,*) "IANG, IFREQ RECEIVED", FLDDESC%IFREQ, FLDDESC%IANGLE
+      ASSOCIATE( &
+            WAMPAR => YDIOS%MODELPAR%YWAM, &
+            ECPAR  => YDIOS%MODELPAR%YECGRIB, &
+            WAMHDR => FLDDESC%YWAM &
+      )
 
+      WRITE(*,*) 'Writing wave model spectral field from IO server.'
 
-      ! What do I need to do encoding?
-      ! ZMISS (which should be applied earlier)
-      ! CDATE, IFCST, MARSTYPE
+      ! Update GRIB handles
+      ! CALL WVCOUPLE_UPDATE_GRIB_HANDLES( .TRUE., WAMPAR%NSTPW, ECPAR%TSTEP, FLDDESC%YWAM%NSTEP, ECPAR%LPPSTEPS, KGRIB_HANDLE)
 
-      ! (got) NGX, NGY, FIELD, ITABLE, IPARAM, IK, IM 
+      IGRIBCD=165
+      ILEV=0
+      CLREPR='GG'
+      CLTYPE='SFC'
+      ! CALL GRIB_CODE_MESSAGE(TSTEP,KGRIB_HANDLE,IGRIBCD,ILEV,CLREPR,CLTYPE,&
+      !       & LLGRAD_DUM,ITOP_DUM,IBOT_DUM)
+      CALL GRIB_SET_TIME( &
+            KGRIB_HANDLE, &
+            ECPAR%LPPSTEPS, &
+            FLDDESC%YWAM%NSTEP,&
+            ECPAR%TSTEP, &
+            ECPAR%NSTEPINI, &
+            ECPAR%LVAREPS, ECPAR%NLEG, &
+            ECPAR%NFCHO_TRUNC_INI, ECPAR%NFCLENGTH_INI, &
+            ECPAR%NREFERENCE, ECPAR%NSTREAM,&
+            & 'fc',FLDDESC%IPREVPP, 165)
 
-      ! Fixed
-      ! IPARAM=251
-      ! ITABLE=140
+      CALL GRIB_SET_PARAMETER(KGRIB_HANDLE,IGRIBCD,ILEV,&
+            & IDUMMY,IDUMMY,IDUMMY,IDUMMY,ZDUMMY)
 
-      ! UPDATE GRIB HANDLES
+      ZDUMMY=0.0_JPRB
+      CALL IGRIB_SET_VALUE(KGRIB_HANDLE,'values',ZDUMMY)
 
-      !CALL WGRIBENCODE_IO_SERV( NGX, NGY, FIELD, ITABLE, IPARAM, 0, IK, IM, CDATE, IFCST, MARSTYPE, KGRIB_HANDLE  )
+      ! Do field-specific GRIB encoding
+      CALL WGRIBENCODE_IO_SERV( WAMPAR%NGX, WAMPAR%NGY, FIELD, YDIOS, FLDDESC, KGRIB_HANDLE  )
 
+      CALL IGRIB_GET_MESSAGE_SIZE( KGRIB_HANDLE, KBYTES )
 
-      !            CALL WGRIBENCODE(IU06, ITEST, NGX, NGY, FIELD,
-      ! &                     ITABLE, IPARAM, 0, IK , IM, 
-      ! &                     CDATE, IFCST, MARSTYPE,
-      ! &                     IGRIB_HANDLE)
+      ISIZE = ( KBYTES + WAMPAR%NPRECI - 1 ) / WAMPAR%NPRECI
 
-      ! Copy to ISENDMSG
+      ALLOCATE( KGRIB_BUFR( ISIZE ) )
 
-      !               CALL WGRIBOUT(IU06, ITEST,
-      !        &                    LFDBIOOUT, CFDB2DSP, NWFDBREF, LFDBOPEN,
-      !        &                    IUOUT,
-     !         &                    IGRIB_HANDLE,MSGSIZE(IRANK),ISENDMSG(1))
+      CALL IGRIB_GET_MESSAGE( KGRIB_HANDLE, KGRIB_BUFR )
 
-      ! Deallocate ISENDMSG
+      ! Use the the FDB opened by the IO server
+      CALL WGRIB2FDB ( &
+            WAMPAR%IU06, WAMPAR%ITEST, &
+            KGRIB_HANDLE, ISIZE, KGRIB_BUFR, &
+            WAMPAR%CFDB2DSP, ECPAR%NFDBREF, &
+            .TRUE., &
+            WAMPAR%IMDLGRBID_G, WAMPAR%IMDLGRBID_M, &
+            IERR &
+      )
 
-      ! GRIBD RELEASE HANDLE
+      IF(IERR.NE.0)THEN
+          WRITE(WAMPAR%IU06,*) ' ------------------------'
+          WRITE(WAMPAR%IU06,*) ' ERROR ACCESSING FDB '
+          WRITE(WAMPAR%IU06,*) ' FDB ERROR CODE IS ',IERR
+          WRITE(WAMPAR%IU06,*) ' ------------------------'
+          WRITE(*,*) ' ------------------------'
+          WRITE(*,*) ' ERROR ACCESSING FDB '
+          WRITE(*,*) ' FDB ERROR CODE IS ',IERR
+          WRITE(*,*) ' ------------------------'
+          CALL ABORT1
+      ENDIF
 
-      ! Or this?
-      !CALL IGRIB_NEW_FROM_MESSAGE(IGRBHNDL,IRECVMSG(1:MSIZE,IRCV))
-      !CALL WGRIBOUT(IU06, ITEST,
-      !&                    LFDBIOOUT, CFDB2DSP, NWFDBREF, LFDBOPEN,
-      !&                    IUOUT,
-      !&                    IGRBHNDL,MSIZE,IRECVMSG(1,IRCV))
+      DEALLOCATE( KGRIB_BUFR )
 
-      ! CALL IGRIB_CLOSE_FILE(IUOUT)
-
-      
+      END ASSOCIATE
 
 #ifdef ECMWF
       IF (LHOOK) CALL DR_HOOK('OUTWSPEC_IO_SERV_REC',1,ZHOOK_HANDLE)
