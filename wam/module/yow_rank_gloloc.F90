ab019118e681715e2f70f1f16c167fb3edb00c28
Samuel Remy <disr@ecmwf.int>
2018-04-23 11:59:59 +0000

    dd

--- wam/module/yow_rank_gloloc.F90
+++
@@ -1,7 +0,0 @@
-MODULE YOW_RANK_GLOLOC
-#if defined MODEL_COUPLING_ATM_WAV || defined MODEL_COUPLING_OCN_WAV
-  USE coupling_var, ONLY : MyRankGlobal, MyRankLocal
-#else
-  integer :: MyRankGlobal, MyRankLocal
-#endif
-END MODULE
