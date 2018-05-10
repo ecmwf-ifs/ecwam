f0cbd937630243408c8fe0ecc5e68bac0c46f3f5
Jean Bidlot <jean.bidlot@ecmwf.int>
2018-05-10 14:32:50 +0100

    out_onegrdpt_sp.F > out_onegrdpt_sp.F90

--- wam/Wam_oper/out_onegrdpt_sp.F90
+++ wam/Wam_oper/out_onegrdpt_sp.F90
@@ -20,6 +20,7 @@
 !           *USTAR*  -   FRICTION VELOCITY
 !           *CDTPRO* -   DATE TIME GROUP
 !--------------------------------------------------------------------
+      USE PARKIND_WAVE, ONLY : JWIM, JWRB, JWRU
  
       USE YOWFRED  , ONLY : FR       ,DELTH    ,TH       ,FRATIO
       USE YOWPARAM , ONLY : NANG     ,NFRE     
@@ -28,29 +29,28 @@
 ! ----------------------------------------------------------------------
       IMPLICIT NONE
  
-      REAL, DIMENSION(NANG,NFRE), INTENT(IN) :: SPEC
-      REAL, INTENT(IN) :: USTAR
+      REAL(KIND=JWRB), DIMENSION(NANG,NFRE), INTENT(IN) :: SPEC
+      REAL(KIND=JWRB), INTENT(IN) :: USTAR
 
       CHARACTER(LEN=12), INTENT(IN) :: CDTPRO
 
-      INTEGER ::  K, M
-      INTEGER :: IU_SPEC
-      INTEGER :: MMAX
-      REAL :: SUMMAX 
-      REAL :: FPK, X1, X2, X3, X4, X5, X6, AF, ASPF, AF2, ASPF2
-      REAL, DIMENSION(NFRE) :: SPF
+      INTEGER(KIND=JWIM) ::  K, M
+      INTEGER(KIND=JWIM) :: IU_SPEC
+      INTEGER(KIND=JWIM) :: MMAX
+      REAL(KIND=JWRB) :: SUMMAX 
+      REAL(KIND=JWRB) :: FPK, X1, X2, X3, X4, X5, X6, AF, ASPF, AF2, ASPF2
+      REAL(KIND=JWRB), DIMENSION(NFRE) :: SPF
 
       CHARACTER(LEN=72) :: CPATH
 
       LOGICAL, SAVE :: FRSTIME
 
       DATA FRSTIME/.TRUE./   
-!
+ 
 ! -------------------------------------------------------------------
-!
+ 
 !       THE SWAMP CASE HAS BEEN REDUCED TO A ONE GRID POINT MODEL
 !       NECESSARY PARAMETERS ARE OUTPUT HERE
-!
 
       IU_SPEC = 998
 
@@ -61,13 +61,13 @@
       ENDIF
          
       DO M=1,NFRE
-        SPF(M) = 0.
+        SPF(M) = 0._JWRB
         DO K=1,NANG 
           SPF(M) = SPF(M) + SPEC(K,M)*DELTH
         ENDDO
       ENDDO 
 
-      SUMMAX = 0.
+      SUMMAX = 0._JWRB
       DO M=1,NFRE
         IF(SPF(M).GT.SUMMAX)THEN
           SUMMAX = SPF(M)
@@ -75,8 +75,8 @@
         END IF
       ENDDO
 
-      IF(SUMMAX.EQ.0.)THEN
-         FPK=0.
+      IF(SUMMAX.EQ.0._JWRB)THEN
+         FPK=0._JWRB
       ELSEIF(MMAX.EQ.1) THEN
          FPK=FR(MMAX)
       ELSEIF(MMAX.EQ.NFRE) THEN
@@ -88,7 +88,7 @@
          X4 = FR(MMAX-1)**2-FR(MMAX+1)**2
          X5 = FR(MMAX-1)-FR(MMAX+1)
          X6 = SPF(MMAX-1)-SPF(MMAX+1)
-         FPK =0.5*X1*(X3*X4-X1*X6)/(X1*X3*X5-X1*X2*X6)
+         FPK =0.5_JWRB*X1*(X3*X4-X1*X6)/(X1*X3*X5-X1*X2*X6)
       END IF 
 
       DO M=1,NFRE
