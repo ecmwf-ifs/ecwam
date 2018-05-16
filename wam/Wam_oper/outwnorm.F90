67a782d03a32c9b333da4e7da37cada8f80cc997
Jean Bidlot <jean.bidlot@ecmwf.int>
2018-05-16 11:34:06 +0100

    fix wave energy flux direction

--- wam/Wam_oper/outwnorm.F90
+++ wam/Wam_oper/outwnorm.F90
@@ -51,29 +51,24 @@
 
         IRECV=1
 !       WRITE NORM TO LOGFILE
-        IF(IRANK.EQ.IRECV) THEN
           WRITE(IU06,*) ' ' 
           IF(LDREPROD) THEN
             WRITE(IU06,*) '  WAMNORM ON ',CDTPRO
-            WRITE(IU06,*) '  !! REPRODUCEABLE IF SAME NPROC !!'
+            WRITE(IU06,*) '  !!!!!!!!! REPRODUCEABLE ONLY IF SAME NPROC'
           ELSE
-            WRITE(IU06,*) '  NON reproduceable WAMNORM ON ',CDTPRO
+            WRITE(IU06,*) '  !!!!!!!!!!!! NON reproduceable WAMNORM ON ',CDTPRO
           ENDIF
           WRITE(IU06,*) '          AVERAGE               MINIMUM  ',    &
-     &     '           MAXIMUM        NON MISSING POINTS      NPROC'
+     &     '           MAXIMUM        NON MISSING POINTS  NPROC'
           DO ITG=1,JPPFLAG
             IF(NFLAG(ITG)) THEN
               IT = ITOBOUT(ITG)
-              WRITE(IU06,*) COUTNAME(ITG)
-              WRITE(IU06,*) '  ',(WNORM(I,IT),I=1,4), NPROC
-              WRITE(IU06,111) (WNORM(I,IT),I=1,4), NPROC
+              WRITE(IU06,*) '  WAMNORM FOR ',COUTNAME(ITG)
+              WRITE(IU06,*) '  ',(WNORM(I,IT),I=1,3),INT(WNORM(I,IT)),NPROC
+              WRITE(IU06,111) (WNORM(I,IT),I=1,3),INT(WNORM(I,IT)),NPROC
             ENDIF
           ENDDO
-        ELSE
-          WRITE(IU06,*) ' ' 
-          WRITE(IU06,*) '   WAMNORM ON ',CDTPRO,' SEE OUTPUT PE',IRECV
-        ENDIF
-111     FORMAT(4x,'HEX: ',4(Z16.16,2x),1x,i6)
+111     FORMAT(4x,'HEX: ',3(Z16.16,2x),1x,i5,1x,i6)
         CALL FLUSH(IU06)
 
       ENDIF
