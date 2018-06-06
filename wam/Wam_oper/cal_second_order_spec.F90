efdd4dadf19b7be29f14d3f58085c6bafbcd0dd5
Jean Bidlot <jean.bidlot@ecmwf.int>
2018-06-06 16:15:00 +0100

    cal_second_order_spec.F > cal_second_order_spec.F90

--- wam/Wam_oper/cal_second_order_spec.F90
+++ wam/Wam_oper/cal_second_order_spec.F90
@@ -1,21 +1,18 @@
-!
-!-----------------------------------------------------------------------
-!
-      SUBROUTINE CAL_SECOND_ORDER_SPEC(F1,IJS,IJL,DEPTH,SIG)
-!
+SUBROUTINE CAL_SECOND_ORDER_SPEC(F1,IJS,IJL,DEPTH,SIG)
+ 
 !***  *CAL_SEC_ORDER_SPEC*   DETERMINES SECOND_ORDER SPECTRUM
-!
+ 
 !     PETER JANSSEN JULY 2010
-!
+ 
 !     PURPOSE. 
 !     --------
-!
+ 
 !              DETERMINATION OF SECOND-ORDER SPECTRUM
-!
+ 
 !     INTERFACE.
 !     ----------
 !              *CALL*  *CAL_SEC_ORDER_SPEC(F1,IJS,IJL,DEPTH,SIG)*
-!
+ 
 !                       INPUT:
 !                            *F1*    - 2-D FREE WAVE SPECTRUM (at input)
 !                            *IJS*   - FIRST GRIDPOINT              
@@ -24,28 +21,30 @@
 !                            *SIG*   - DIRECTION OF MAPPING:
 !                                      FORWARD: SIG = +1
 !                                      INVERSE: SIG = -1
-!
+ 
 !                       OUTPUT: 
 !                            *F1*    - 2-D SPECTRUM INCLUDING SECOND-ORDER
 !                                      CORRECTION (at output)
-!
+ 
 !     METHOD.
 !     -------
 !              IS DESCRIBED IN JANSSEN (2009), JFM, 637, 1-44.
-!
+ 
 !     EXTERNALS.
 !     ----------
 !              SECSPOM
-!
+ 
 !-----------------------------------------------------------------------
-!
+ 
+      USE PARKIND_WAVE, ONLY : JWIM, JWRB, JWRU
+
       USE YOWFRED, ONLY: FR, DFIM, DELTH,TH,FRATIO
       USE YOMHOOK   ,ONLY : LHOOK,   DR_HOOK
       USE YOWPARAM, ONLY: NANG,NFRE,CLDOMAIN
       USE YOWPCONS, ONLY: G, PI, ZPI
       USE YOWSHAL, ONLY: NDEPTH,DEPTHA,DEPTHD
-      USE YOWTABL, ONLY : MR, XMR, MA, XMA, NFREH, NANGH, NMAX, 
-     &                    OMEGA, DFDTH, THH, DELTHH, IM_P, IM_M, 
+      USE YOWTABL, ONLY : MR, XMR, MA, XMA, NFREH, NANGH, NMAX,         &
+     &                    OMEGA, DFDTH, THH, DELTHH, IM_P, IM_M,        &
      &                    TA, TB, TC_QL, TT_4M, TT_4P
       USE YOWTEST  , ONLY : IU06
 
@@ -55,67 +54,64 @@
 #include "fkmean.intfb.h"
 #include "secspom.intfb.h"
 
-      INTEGER, INTENT(IN) :: IJS, IJL
-      REAL, DIMENSION(IJS:IJL), INTENT(IN) :: DEPTH
-      REAL, DIMENSION(IJS:IJL,1:NANG,1:NFRE), INTENT(INOUT) :: F1
-      REAL, INTENT(IN) :: SIG
+      INTEGER(KIND=JWIM), INTENT(IN) :: IJS, IJL
+      INTEGER(KIND=JWIM) :: IJ,M,K,K0,M0,MP,KP,MM,KM,KL,KLL,ML
 
-      INTEGER :: IJ,M,K,K0,M0,MP,KP,MM,KM,KL,KLL,ML
+      REAL(KIND=JWRB), INTENT(IN) :: SIG
+      REAL(KIND=JWRB), DIMENSION(IJS:IJL), INTENT(IN) :: DEPTH
+      REAL(KIND=JWRB), DIMENSION(IJS:IJL,1:NANG,1:NFRE), INTENT(INOUT) :: F1
 
-      REAL :: FRAC,CO1,DEL,DELF,D1,D2,D3,D4,C1
-      REAL :: C2,XM,XK,OMSTART,AREA,SUM,SUM1,SUM3,GAM_B_J,ZFAC
-      REAL :: ZHOOK_HANDLE
-      REAL, DIMENSION(IJS:IJL) :: EMEANALL, FMEANALL
-      REAL, DIMENSION(IJS:IJL) :: F1MEAN, AKMEAN, XKMEAN, EMAXL  
-      REAL, DIMENSION(IJS:IJL,NANG,NFRE) :: F3
-      REAL, ALLOCATABLE ::  PF1(:,:,:),PF3(:,:,:)
+      REAL(KIND=JWRB) :: FRAC,CO1,DEL,DELF,D1,D2,D3,D4,C1
+      REAL(KIND=JWRB) :: C2,XM,XK,OMSTART,AREA,SUM,SUM1,SUM3,GAM_B_J,ZFAC
+      REAL(KIND=JWRB) :: ZHOOK_HANDLE
+      REAL(KIND=JWRB), DIMENSION(IJS:IJL) :: EMEANALL, FMEANALL
+      REAL(KIND=JWRB), DIMENSION(IJS:IJL) :: F1MEAN, AKMEAN, XKMEAN, EMAXL  
+      REAL(KIND=JWRB), DIMENSION(IJS:IJL,NANG,NFRE) :: F3
+      REAL(KIND=JWRB), ALLOCATABLE ::  PF1(:,:,:),PF3(:,:,:)
 
 !-----------------------------------------------------------------------
-#ifdef ECMWF
+
       IF (LHOOK) CALL DR_HOOK('CAL_SECOND_ORDER_SPEC',0,ZHOOK_HANDLE)
-#endif
-!
+ 
 !***  1. DETERMINE SECOND ORDER CORRECTION TO THE SPECTRUM
 !     ----------------------------------------------------
-!
-      GAM_B_J = 0.6
-      ZFAC = GAM_B_J**2/16.
-      FRAC = FRATIO-1.
+ 
+      GAM_B_J = 0.6_JWRB
+      ZFAC = GAM_B_J**2/16.0_JWRB
+      FRAC = FRATIO-1.0_JWRB
       OMSTART = ZPI*FR(1)
 
-      CALL FKMEAN(F1,IJS,IJL,EMEANALL,FMEANALL,F1MEAN,AKMEAN,
-     &            XKMEAN)
-!
+      CALL FKMEAN(F1,IJS,IJL,EMEANALL,FMEANALL,F1MEAN,AKMEAN,XKMEAN)
+ 
 !***  1.1 INTERPOLATION OR NOT.
 !     ------------------------
-!
+ 
       IF (MR.EQ.1 .AND. MA.EQ.1) THEN
-!
+ 
 !***     1.11 NO INTERPOLATION.
 !        ----------------------
-! 
-         CALL SECSPOM(F1,F3,IJS,IJL,NFRE,NANG,NMAX,NDEPTH,DEPTHA,
-     &                DEPTHD,OMSTART,FRAC,MR,DFDTH,OMEGA,DEPTH,
+  
+         CALL SECSPOM(F1,F3,IJS,IJL,NFRE,NANG,NMAX,NDEPTH,DEPTHA,       &
+     &                DEPTHD,OMSTART,FRAC,MR,DFDTH,OMEGA,DEPTH,         &
      &                AKMEAN,TA,TB,TC_QL,TT_4M,TT_4P,IM_P,IM_M)
          DO M=1,NFRE
             DO K=1,NANG
                DO IJ=IJS,IJL
                   DELF = F3(IJ,K,M)
-                  F1(IJ,K,M)=MAX(MIN(0.000001,F1(IJ,K,M)),
-     &                               F1(IJ,K,M)+SIG*DELF)
+                  F1(IJ,K,M)=MAX(MIN(0.000001_JWRB,F1(IJ,K,M)),F1(IJ,K,M)+SIG*DELF)
                ENDDO
             ENDDO
          ENDDO
 
       ELSE
 
-!
+ 
 !***     1.12 ENERGY CONSERVING INTERPOLATION SCHEME
 !        -------------------------------------------
-!
+ 
          ALLOCATE(PF1(IJS:IJL,NANGH,NFREH))
          ALLOCATE(PF3(IJS:IJL,NANGH,NFREH))
-         PF1(:,:,:) = 0.
+         PF1(:,:,:) = 0._JWRB
          DO M=1,NFREH
             M0 = MR*M
             DO K=1,NANGH
@@ -128,22 +124,21 @@
             ENDDO
          ENDDO
 
-!
 !***     1.13 DETERMINE SECOND-ORDER SPEC
 !        --------------------------------
- 
-         CALL SECSPOM(PF1,PF3,IJS,IJL,NFREH,NANGH,NMAX,NDEPTH,DEPTHA,
-     &                DEPTHD,OMSTART,FRAC,MR,DFDTH,OMEGA,DEPTH,
+
+         CALL SECSPOM(PF1,PF3,IJS,IJL,NFREH,NANGH,NMAX,NDEPTH,DEPTHA,   &
+     &                DEPTHD,OMSTART,FRAC,MR,DFDTH,OMEGA,DEPTH,         &
      &                AKMEAN,TA,TB,TC_QL,TT_4M,TT_4P,IM_P,IM_M)
-!
+ 
 !***     2.24 INTERPOLATE TOWARDS HIGH-RES GRID
 !        --------------------------------------
-!
+ 
          DO IJ=IJS,IJL
            IF (EMEANALL(IJ).LE.ZFAC*DEPTH(IJ)**2) THEN 
-             EMAXL(IJ)=1.
+             EMAXL(IJ)=1._JWRB
            ELSE
-             EMAXL(IJ)=0.
+             EMAXL(IJ)=0._JWRB
            ENDIF
          ENDDO
 
@@ -154,14 +149,14 @@
             IF(M0.LT.1) THEN
               M0 = 1
               MP = M0+1
-              D1 = 1.
+              D1 = 1._JWRB
             ELSE IF(M0.LT.NFREH) THEN
               MP = M0+1
               D1 = (FR(M)-FR(MR*M0))/(FR(MR*MP)-FR(MR*M0)) 
             ELSE
               M0 = NFREH
               MP = M0
-              D1 = 0.
+              D1 = 0._JWRB
             ENDIF
             D2 = 1.-D1
 
@@ -179,7 +174,7 @@
                   C2 = PF3(IJ,KP,MP)*D3+PF3(IJ,K0,MP)*D4
                   DELF = C1*D2+C2*D1
 
-                  F1(IJ,K,M)=MAX(MIN(0.000001,F1(IJ,K,M)),
+                  F1(IJ,K,M)=MAX(MIN(0.000001_JWRB,F1(IJ,K,M)),         &
      &                               F1(IJ,K,M)+EMAXL(IJ)*SIG*DELF)
 
                ENDDO
@@ -188,9 +183,7 @@
       ENDIF
       IF (ALLOCATED(PF1)) DEALLOCATE(PF1)
       IF (ALLOCATED(PF3)) DEALLOCATE(PF3)
-!      
-#ifdef ECMWF
-      IF (LHOOK) CALL DR_HOOK('CAL_SECOND_ORDER_SPEC',1,ZHOOK_HANDLE)
-#endif
 
-      END SUBROUTINE CAL_SECOND_ORDER_SPEC
+      IF (LHOOK) CALL DR_HOOK('CAL_SECOND_ORDER_SPEC',1,ZHOOK_HANDLE)
+
+END SUBROUTINE CAL_SECOND_ORDER_SPEC
