f62b6f654187848db816df3bc870cb1998db164d
Jean Bidlot <jean.bidlot@ecmwf.int>
2018-06-20 14:17:48 +0100

    fix sepwisw to always computes all parameters

--- wam/Wam_oper/sepwisw.F90
+++ wam/Wam_oper/sepwisw.F90
@@ -1,9 +1,9 @@
-      SUBROUTINE SEPWISW (IJS, IJL, MIJ, FL3, XLLWS,
-     &                    USNEW, U10NEW, THWNEW,
-     &                    ESWELL   ,FSWELL   ,THSWELL  ,
-     &                    P1SWELL  ,P2SWELL  ,SPRDSWELL,
-     &                    ESEA     ,FSEA     ,THWISEA  ,
-     &                    P1SEA    ,P2SEA    ,SPRDSEA  ,
+      SUBROUTINE SEPWISW (IJS, IJL, MIJ, FL3, XLLWS,                    &
+     &                    USNEW, U10NEW, THWNEW,                        &
+     &                    ESWELL   ,FSWELL   ,THSWELL  ,                &
+     &                    P1SWELL  ,P2SWELL  ,SPRDSWELL,                &
+     &                    ESEA     ,FSEA     ,THWISEA  ,                &
+     &                    P1SEA    ,P2SEA    ,SPRDSEA  ,                &
      &                    EMTRAIN  ,THTRAIN  ,PMTRAIN)
 
 ! ----------------------------------------------------------------------
@@ -63,7 +63,9 @@
 
 ! ----------------------------------------------------------------------
 
-      USE YOWCOUT  , ONLY : IPFGTBL  ,NTRAIN   ,LLPARTITION
+      USE PARKIND_WAVE, ONLY : JWIM, JWRB, JWRU
+
+      USE YOWCOUT  , ONLY : NTRAIN   ,LLPARTITION
       USE YOWFRED  , ONLY : C        ,TH       ,FRIC     ,OLDWSFC
       USE YOWPCONS , ONLY : EPSMIN
       USE YOWPARAM , ONLY : NANG     ,NFRE     ,CLDOMAIN
@@ -79,40 +81,32 @@
 #include "sthq.intfb.h"
 #include "wdirspread.intfb.h"
 
-      INTEGER, INTENT(IN) :: IJS,IJL
-      INTEGER, DIMENSION(IJS:IJL), INTENT(IN) :: MIJ
-      REAL, DIMENSION(IJS:IJL), INTENT(IN) :: USNEW, U10NEW, THWNEW
-      REAL, DIMENSION(IJS:IJL), INTENT(OUT) :: ESWELL ,FSWELL ,THSWELL
-      REAL, DIMENSION(IJS:IJL), INTENT(OUT) :: P1SWELL,P2SWELL,SPRDSWELL
-      REAL, DIMENSION(IJS:IJL), INTENT(OUT) :: ESEA   ,FSEA   ,THWISEA
-      REAL, DIMENSION(IJS:IJL), INTENT(OUT) :: P1SEA  ,P2SEA  ,SPRDSEA
-      REAL, DIMENSION(IJS:IJL,NTRAIN), INTENT(OUT) :: EMTRAIN
-      REAL, DIMENSION(IJS:IJL,NTRAIN), INTENT(OUT) :: THTRAIN, PMTRAIN
-      REAL, DIMENSION(IJS:IJL,NANG,NFRE), INTENT(IN) :: FL3
-      REAL, DIMENSION(IJS:IJL,NANG,NFRE), INTENT(IN) :: XLLWS 
+      INTEGER(KIND=JWIM), INTENT(IN) :: IJS,IJL
+      INTEGER(KIND=JWIM), DIMENSION(IJS:IJL), INTENT(IN) :: MIJ
 
-      INTEGER :: IJ,K,M
+      REAL(KIND=JWRB), DIMENSION(IJS:IJL), INTENT(IN) :: USNEW, U10NEW, THWNEW
+      REAL(KIND=JWRB), DIMENSION(IJS:IJL), INTENT(OUT) :: ESWELL ,FSWELL ,THSWELL, P1SWELL, P2SWELL, SPRDSWELL
+      REAL(KIND=JWRB), DIMENSION(IJS:IJL), INTENT(OUT) :: ESEA   ,FSEA   ,THWISEA, P1SEA  , P2SEA  , SPRDSEA
+      REAL(KIND=JWRB), DIMENSION(IJS:IJL,NTRAIN), INTENT(OUT) :: EMTRAIN, THTRAIN, PMTRAIN
+      REAL(KIND=JWRB), DIMENSION(IJS:IJL,NANG,NFRE), INTENT(IN) :: FL3
+      REAL(KIND=JWRB), DIMENSION(IJS:IJL,NANG,NFRE), INTENT(IN) :: XLLWS
 
-      REAL :: COEF
-      REAL :: CHECKTA
-      REAL :: ZHOOK_HANDLE
-      REAL, DIMENSION(NFRE) :: CM
-      REAL, DIMENSION(IJS:IJL) :: R
-      REAL, DIMENSION(IJS:IJL,NFRE) :: XINVWVAGE
-      REAL, DIMENSION(IJS:IJL,NANG) :: DIRCOEF
-      REAL, DIMENSION(IJS:IJL,NANG,NFRE) :: SWM, FL1
+      INTEGER(KIND=JWIM) :: IJ, K, M
+
+      REAL(KIND=JWRB) :: COEF
+      REAL(KIND=JWRB) :: CHECKTA
+      REAL(KIND=JWRB) :: ZHOOK_HANDLE
+      REAL(KIND=JWRB), DIMENSION(NFRE) :: CM
+      REAL(KIND=JWRB), DIMENSION(IJS:IJL) :: R
+      REAL(KIND=JWRB), DIMENSION(IJS:IJL,NFRE) :: XINVWVAGE
+      REAL(KIND=JWRB), DIMENSION(IJS:IJL,NANG) :: DIRCOEF
+      REAL(KIND=JWRB), DIMENSION(IJS:IJL,NANG,NFRE) :: SWM, FL1
 
       LOGICAL :: LLPEAKF
 
 ! ----------------------------------------------------------------------
 
-#ifdef ECMWF
       IF (LHOOK) CALL DR_HOOK('SEPWISH',0,ZHOOK_HANDLE)
-#endif
-
-      ESWELL(:)=0.0
-      ESEA(:)=0.0
-      EMTRAIN(:,:)=0.0
 
 !*    1. THE SWELL DISTRIBUTION IS COMPUTED.
 !        -----------------------------------
@@ -120,7 +114,7 @@
       COEF = OLDWSFC*FRIC
 
       DO M=1,NFRE
-        CM(M)=1.0/C(M)
+        CM(M)=1.0_JWRB/C(M)
         DO IJ=IJS,IJL
           XINVWVAGE(IJ,M)=USNEW(IJ)*CM(M)
         ENDDO
@@ -135,17 +129,17 @@
       DO M=1,NFRE
         DO K=1,NANG
           DO IJ=IJS,IJL
-            IF (XLLWS(IJ,K,M).NE.0. .AND. .NOT.(CLDOMAIN.EQ.'s') ) THEN
+            IF (XLLWS(IJ,K,M).NE.0.0_JWRB) THEN
               ! this is windsea 
-              SWM(IJ,K,M)=0.
+              SWM(IJ,K,M)=0.0_JWRB
             ELSE
               CHECKTA=XINVWVAGE(IJ,M)*DIRCOEF(IJ,K)
-              IF (CHECKTA.GE.1.) THEN
+              IF (CHECKTA.GE.1.0_JWRB) THEN
                 ! this is extra windsea 
-                SWM(IJ,K,M)=0.
+                SWM(IJ,K,M)=0.0_JWRB
               ELSE
                 ! this is swell
-                SWM(IJ,K,M)=1.
+                SWM(IJ,K,M)=1.0_JWRB
               ENDIF
             ENDIF
           ENDDO
@@ -170,7 +164,7 @@
       DO M=1,NFRE
         DO K=1,NANG
           DO IJ=IJS,IJL
-            FL1(IJ,K,M)=MAX(FL3(IJ,K,M)-FL1(IJ,K,M),0.)
+            FL1(IJ,K,M)=MAX(FL3(IJ,K,M)-FL1(IJ,K,M),0.0_JWRB)
           ENDDO
         ENDDO
       ENDDO
@@ -179,24 +173,24 @@
 !     CHECK:
       DO IJ=IJS,IJL
          IF(FSWELL(IJ).GT.FSEA(IJ)) THEN
-           R(IJ)=1.
+           R(IJ)=1.0_JWRB
          ELSE
-           R(IJ)=0.
+           R(IJ)=0.0_JWRB
          ENDIF
       ENDDO
       DO K=1,NANG
         DO IJ=IJS,IJL
           ! add factor to extend windsea area
-          DIRCOEF(IJ,K)=R(IJ)*COEF*SIGN(1.,0.4+COS(TH(K)-THWNEW(IJ)))
+          DIRCOEF(IJ,K)=R(IJ)*COEF*SIGN(1.0_JWRB,0.4_JWRB+COS(TH(K)-THWNEW(IJ)))
         ENDDO
       ENDDO
       DO M=1,NFRE
         DO K=1,NANG
           DO IJ=IJS,IJL
             CHECKTA = XINVWVAGE(IJ,M)*DIRCOEF(IJ,K)
-            IF (CHECKTA.GE.1.) THEN
+            IF (CHECKTA.GE.1.0_JWRB) THEN
               ! this is additional windsea 
-              SWM(IJ,K,M)=0.
+              SWM(IJ,K,M)=0.0_JWRB
             ENDIF
           ENDDO
         ENDDO
@@ -207,10 +201,10 @@
       DO IJ=IJS,IJL
         DO K=1,NANG
           DO M=NFRE,2,-1
-            IF(SWM(IJ,K,M).EQ.1. .AND. SWM(IJ,K,M-1).EQ.1.) THEN
+            IF(SWM(IJ,K,M).EQ.1.0_JWRB .AND. SWM(IJ,K,M-1).EQ.1.0_JWRB) THEN
               EXIT
-            ELSEIF(SWM(IJ,K,M).EQ.0. .AND. SWM(IJ,K,M-1).EQ.1.) THEN
-               IF(FL3(IJ,K,M).GE.FL3(IJ,K,M-1)) SWM(IJ,K,M-1)=0. 
+            ELSEIF(SWM(IJ,K,M).EQ.0.0_JWRB .AND. SWM(IJ,K,M-1).EQ.1.0_JWRB) THEN
+               IF(FL3(IJ,K,M).GE.FL3(IJ,K,M-1)) SWM(IJ,K,M-1)=0.0_JWRB
             ENDIF
           ENDDO
         ENDDO
@@ -233,36 +227,29 @@
       IF(LLPARTITION) THEN
         CALL FEMEAN(FL1, IJS, IJL, ESWELL, FSWELL)
         CALL STHQ(FL1, IJS, IJL, THSWELL)
-        CALL SEP3TR (FL3, IJS, IJL, MIJ, U10NEW, THWNEW,
-     &               ESWELL, FSWELL, THSWELL, FSEA,
-     &               FL1, SWM,
+        CALL SEP3TR (FL3, IJS, IJL, MIJ, U10NEW, THWNEW,                &
+     &               ESWELL, FSWELL, THSWELL, FSEA,                     &
+     &               FL1, SWM,                                          &
      &               EMTRAIN  ,THTRAIN  ,PMTRAIN)
+      ELSE
+        EMTRAIN(:,:)=0.0_JWRB
+        THTRAIN(:,:)=0.0_JWRB
+        PMTRAIN(:,:)=0.0_JWRB
       ENDIF
 
 !*    2.2 COMPUTATION OF TOTAL SWELL OUTPUT PARAMETERS
 !         --------------------------------------------
 
-      IF(IPFGTBL(12).NE.0 .OR. IPFGTBL(16).NE.0 .OR.
-     &   IPFGTBL(29).NE.0 .OR. IPFGTBL(31).NE.0 ) THEN
-        CALL FEMEAN(FL1, IJS, IJL, ESWELL, FSWELL)
-      ENDIF
+      CALL FEMEAN(FL1, IJS, IJL, ESWELL, FSWELL)
       
-      IF(IPFGTBL(14).NE.0) THEN
-        CALL STHQ(FL1, IJS, IJL, THSWELL)
-      ENDIF
+      CALL STHQ(FL1, IJS, IJL, THSWELL)
 
-      IF(IPFGTBL(29).NE.0) THEN
-        CALL MWP1(FL1, IJS, IJL, ESWELL, P1SWELL)
-      ENDIF
+      CALL MWP1(FL1, IJS, IJL, ESWELL, P1SWELL)
 
-      IF(IPFGTBL(31).NE.0) THEN
-        CALL MWP2(FL1, IJS, IJL, ESWELL, P2SWELL)
-      ENDIF
+      CALL MWP2(FL1, IJS, IJL, ESWELL, P2SWELL)
 
-      IF(IPFGTBL(33).NE.0) THEN
-        LLPEAKF = .TRUE.
-        CALL WDIRSPREAD (FL1, IJS, IJL, ESWELL, LLPEAKF, SPRDSWELL)
-      ENDIF
+      LLPEAKF = .TRUE.
+      CALL WDIRSPREAD (FL1, IJS, IJL, ESWELL, LLPEAKF, SPRDSWELL)
 
 
 !*    3. COMPUTATION OF WIND SEA OUTPUT PARAMETERS
@@ -271,43 +258,29 @@
       DO M=1,NFRE
         DO K=1,NANG
           DO IJ=IJS,IJL
-            FL1(IJ,K,M)=MAX(FL3(IJ,K,M)-FL1(IJ,K,M)+EPSMIN,0.)
+            FL1(IJ,K,M)=MAX(FL3(IJ,K,M)-FL1(IJ,K,M)+EPSMIN,0.0_JWRB)
           ENDDO
         ENDDO
       ENDDO
 
-      IF(IPFGTBL(15).NE.0 .OR. IPFGTBL(11).NE.0 .OR.
-     &   IPFGTBL(13).NE.0 .OR. IPFGTBL(28).NE.0 .OR.
-     &   IPFGTBL(30).NE.0) THEN
-        CALL FEMEAN(FL1, IJS, IJL, ESEA, FSEA)
-      ENDIF
+      CALL FEMEAN(FL1, IJS, IJL, ESEA, FSEA)
 
-      IF(IPFGTBL(13).NE.0) THEN
-        CALL STHQ(FL1, IJS, IJL, THWISEA)
-!       if there isn't any windsea energy, set the windsea mean direction
-!       to the wind direction.
-        DO IJ=IJS,IJL
-          IF(ESEA(IJ).LE.0.) THEN
-            THWISEA(IJ)=THWNEW(IJ)
-          ENDIF
-        ENDDO
-      ENDIF
+      CALL STHQ(FL1, IJS, IJL, THWISEA)
+!     if there isn't any windsea energy, set the windsea mean direction
+!     to the wind direction.
+      DO IJ=IJS,IJL
+        IF(ESEA(IJ).LE.0.0_JWRB) THEN
+          THWISEA(IJ)=THWNEW(IJ)
+        ENDIF
+      ENDDO
 
-      IF(IPFGTBL(28).NE.0) THEN
-        CALL MWP1(FL1, IJS, IJL, ESEA, P1SEA)
-      ENDIF
+      CALL MWP1(FL1, IJS, IJL, ESEA, P1SEA)
 
-      IF(IPFGTBL(30).NE.0) THEN
-        CALL MWP2(FL1, IJS, IJL, ESEA, P2SEA)
-      ENDIF
+      CALL MWP2(FL1, IJS, IJL, ESEA, P2SEA)
 
-      IF(IPFGTBL(32).NE.0) THEN
-        LLPEAKF = .TRUE.
-        CALL WDIRSPREAD(FL1, IJS, IJL, ESEA, LLPEAKF, SPRDSEA)
-      ENDIF
+      LLPEAKF = .TRUE.
+      CALL WDIRSPREAD(FL1, IJS, IJL, ESEA, LLPEAKF, SPRDSEA)
 
-#ifdef ECMWF
       IF (LHOOK) CALL DR_HOOK('SEPWISH',1,ZHOOK_HANDLE)
-#endif
 
       END SUBROUTINE SEPWISW
