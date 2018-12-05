a25e13ef7374e3106b990a0510e5d870d4493088
Jean Bidlot <jean.bidlot@ecmwf.int>
2018-12-05 15:43:22 +0000

    fix single precision problem

--- wam/Wam_oper/updnemofields.F90
+++ wam/Wam_oper/updnemofields.F90
@@ -49,26 +49,21 @@
       USE MPL_DATA_MODULE, ONLY : MPL_COMM
 ! COUPLING INFORMATION
       USE YOWCOUP  , ONLY : LWNEMOCOU, LWNEMOCOUDEBUG,                  &
-     &                      LWNEMOCOUSTK, LWNEMOCOUSTRN, KCOUSTEP
-      USE YOWNEMOP   , ONLY : NEMODP
-      USE YOWNEMOFLDS, ONLY : NEMOSTRN, NEMOUSTOKES, NEMOVSTOKES
-! DATA TO BE SEND
-      USE YOWMEAN  , ONLY : NSWH, NMWP, NPHIEPS, NTAUOC
+     &                      LWNEMOCOUSTK, LWNEMOCOUSTRN, KCOUSTEP,      &
+     &                      NSWH, NMWP, NPHIEPS, NTAUOC,                &
+     &                      NEMOSTRN, NEMOUSTOKES, NEMOVSTOKES
 ! DR. HOOK
       USE YOMHOOK  , ONLY : LHOOK,   DR_HOOK
 ! INFORMATION FOR OPTIONAL DEBUGGING
-      USE YOWSTAT  , ONLY : CDATEF, CDTPRO
+      USE YOWSTAT  , ONLY : CDTPRO
 
 ! -------------------------------------------------------------------   
 
       IMPLICIT NONE
 
-      INTEGER(KIND=JWIM) :: IG, IJ
+      INTEGER(KIND=JWIM) :: IG
 
-      REAL(KIND=JWRB) :: TAU
       REAL(KIND=JWRB) :: ZHOOK_HANDLE
-      REAL(KIND=NEMODP), ALLOCATABLE,DIMENSION(:) :: ZDUMU, ZDUMV, ZDUMSTRN
-
 ! -------------------------------------------------------------------   
 
       IF (LHOOK) CALL DR_HOOK('UPDNEMOFIELDS',0,ZHOOK_HANDLE)
@@ -77,37 +72,16 @@
 
         IG=1
 
-! MEAN SQUARE WAVE STRAIN IN SEA ICE
-      
-        ALLOCATE(ZDUMSTRN(IJS(IG):IJL(IG)))
-        IF (LWNEMOCOUSTRN) THEN
-           ZDUMSTRN(IJS(IG):IJL(IG)) = NEMOSTRN(IJS(IG):IJL(IG))
-        ELSE
-           ZDUMSTRN(IJS(IG):IJL(IG)) = 0.0_NEMODP
-        ENDIF
-  
-! STOKES DRIFT
-
-        ALLOCATE(ZDUMU(IJS(IG):IJL(IG)),ZDUMV(IJS(IG):IJL(IG)))
-        IF (LWNEMOCOUSTK) THEN
-           ZDUMU(IJS(IG):IJL(IG)) = NEMOUSTOKES(IJS(IG):IJL(IG))
-           ZDUMV(IJS(IG):IJL(IG)) = NEMOVSTOKES(IJS(IG):IJL(IG))
-        ELSE
-           ZDUMU(IJS(IG):IJL(IG)) = 0.0_NEMODP
-           ZDUMV(IJS(IG):IJL(IG)) = 0.0_NEMODP
-        ENDIF
-
 #ifdef WITH_NEMO
         CALL NEMOGCMCOUP_WAM_UPDATE( IRANK-1, NPROC, MPL_COMM,          &
      &       IJL(IG)-IJS(IG)+1,                                         &
      &       NSWH(IJS(IG):IJL(IG)), NMWP(IJS(IG):IJL(IG)),              &
      &       NPHIEPS(IJS(IG):IJL(IG)), NTAUOC(IJS(IG):IJL(IG)),         &
-     &       ZDUMSTRN(IJS(IG):IJL(IG)),                                 &
-     &       ZDUMU(IJS(IG):IJL(IG)), ZDUMV(IJS(IG):IJL(IG)),            &
+     &       NEMOSTRN(IJS(IG):IJL(IG)),                                 &
+     &       NEMOUSTOKES(IJS(IG):IJL(IG)), NEMOVSTOKES(IJS(IG):IJL(IG)),&
      &       CDTPRO, LWNEMOCOUDEBUG )
 #endif
 
-        DEALLOCATE(ZDUMSTRN,ZDUMU,ZDUMV)
 
       ENDIF
 
