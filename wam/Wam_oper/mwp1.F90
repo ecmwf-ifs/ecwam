fa37a6b3315ba53a1c904c940b78875dc1a61c47
Jean Bidlot <jean.bidlot@ecmwf.int>
2019-06-12 12:11:34 +0100

    fix for p1ww and p2ww to insure that it uses the same 0 and 1st moments

--- wam/Wam_oper/mwp1.F90
+++ wam/Wam_oper/mwp1.F90
@@ -1,4 +1,4 @@
-      SUBROUTINE MWP1 (F, IJS, IJL, EMEAN, MEANWP1)
+      SUBROUTINE MWP1 (F, IJS, IJL, MEANWP1)
 
 ! ----------------------------------------------------------------------
 
@@ -16,11 +16,10 @@
 !**   INTERFACE.
 !     ----------
 
-!       *CALL* *MWP1 (F, IJS, IJL, EMEAN, MEANWP1)*
+!       *CALL* *MWP1 (F, IJS, IJL, MEANWP1)*
 !              *F*   - SPECTRUM.
 !              *IJS* - INDEX OF FIRST GRIDPOINT
 !              *IJL* - INDEX OF LAST GRIDPOINT
-!              *EMEAN* - MEAN WAVE ENERGY FOR THE WAVE SYSTEM (INPUT).
 !              *MEANWP1* - MEAN PERIOD BASED ON THE FIRST MOMENT.
 
 !     METHOD.
@@ -42,29 +41,29 @@
 
       USE PARKIND_WAVE, ONLY : JWIM, JWRB, JWRU
 
-      USE YOWFRED  , ONLY : FR       ,DFIMFR_SIM,DELTH    ,WP1TAIL
+      USE YOWFRED  , ONLY : FR       ,DFIM_SIM ,DFIMFR_SIM,DELTH  ,WETAIL, WP1TAIL
       USE YOWPARAM , ONLY : NANG     ,NFRE     ,NFRE_ODD
       USE YOWPCONS , ONLY : EPSMIN
-      USE YOMHOOK   ,ONLY : LHOOK,   DR_HOOK
+      USE YOMHOOK  , ONLY : LHOOK,   DR_HOOK
 
 ! ----------------------------------------------------------------------
       IMPLICIT NONE
 
       INTEGER(KIND=JWIM), INTENT(IN) :: IJS, IJL
       REAL(KIND=JWRB), INTENT(IN) :: F(IJS:IJL,NANG,NFRE)
-      REAL(KIND=JWRB), DIMENSION(IJS:IJL), INTENT(IN) :: EMEAN
       REAL(KIND=JWRB), DIMENSION(IJS:IJL), INTENT(OUT) :: MEANWP1
 
       INTEGER(KIND=JWIM) :: IJ, K, M
       REAL(KIND=JWRB) :: ZHOOK_HANDLE
-      REAL(KIND=JWRB) :: COEF_FR
-      REAL(KIND=JWRB), DIMENSION(IJS:IJL) :: TEMP
+      REAL(KIND=JWRB) :: DELT25, COEF_FR, FR1M1
+      REAL(KIND=JWRB), DIMENSION(IJS:IJL) :: TEMP, EM
 
 ! ----------------------------------------------------------------------
 
       IF (LHOOK) CALL DR_HOOK('MWP1',0,ZHOOK_HANDLE)
 
       DO IJ=IJS,IJL
+        EM(IJ) = 0.0_JWRB
         MEANWP1(IJ) = 0.0_JWRB
       ENDDO
 
@@ -78,6 +77,7 @@
           ENDDO
         ENDDO
         DO IJ=IJS,IJL
+          EM(IJ) = EM(IJ)+DFIM_SIM(M)*TEMP(IJ)
           MEANWP1(IJ) = MEANWP1(IJ)+DFIMFR_SIM(M)*TEMP(IJ)
         ENDDO
       ENDDO
@@ -85,12 +85,16 @@
 !*    ADD TAIL CORRECTION TO MEAN PERIOD AND
 !     AND TRANSFORM TO PERIOD.
 
+      FR1M1 = 1.0_JWRB/FR(1)
+      DELT25 = WETAIL*FR(NFRE_ODD)*DELTH
       COEF_FR = WP1TAIL*DELTH*FR(NFRE_ODD)**2
 
       DO IJ=IJS,IJL
+        EM(IJ) = EM(IJ)+DELT25*TEMP(IJ)
         MEANWP1(IJ) = MEANWP1(IJ)+COEF_FR*TEMP(IJ)
-        IF(EMEAN(IJ).GT.EPSMIN .AND. MEANWP1(IJ).GT.EPSMIN ) THEN
-          MEANWP1(IJ) = EMEAN(IJ)/MEANWP1(IJ)
+        IF(EM(IJ).GT.0.0_JWRB .AND. MEANWP1(IJ).GT.EPSMIN ) THEN
+          MEANWP1(IJ) = EM(IJ)/MEANWP1(IJ)
+          MEANWP1(IJ) = MIN(MEANWP1(IJ),FR1M1)
         ELSE
           MEANWP1(IJ) = 0.0_JWRB
         ENDIF
