9d878a3861f55ff553a572773bb14b2d007568d1
Jean Bidlot <jean.bidlot@ecmwf.int>
2020-06-10 09:41:20 +0100

    add tauv in the calculation of zb

--- wam/Wam_oper/taut_z0.F90
+++ wam/Wam_oper/taut_z0.F90
@@ -127,12 +127,12 @@ IF (LLGCBZ0) THEN
         DO ITER=1,NITER
 !         Z0 IS DERIVED FROM THE NEUTRAL LOG PROFILE: UTOP = (USTAR/XKAPPA)*LOG((XNLEV+Z0)/Z0)
           Z0(IJ) = MAX(XNLEV/(EXP(XKUTOP/USTAR(IJ))-1.0_JWRB),Z0MINDYN)
-!         GRAVITY CAPILLARY CONTRIBUTION:
-          CALL STRESS_GC(USTAR(IJ), Z0(IJ), ALPHAP(IJ), XMSS(IJ), TAUUNR(IJ))
-          ZB(IJ) = MAX(Z0(IJ)*SQRT(TAUUNR(IJ)/TAUOLD), Z0MINDYN)
-!         TOTAL kinematic STRESS:
           ! Viscous kinematic stress nu_air * dU/dz at z=0 of the neutral log profile reduced by factor 25 (0.04)
           TAUV = RNUEFF*USTAR(IJ)/(XKAPPA*Z0(IJ))
+!         GRAVITY CAPILLARY CONTRIBUTION:
+          CALL STRESS_GC(USTAR(IJ), Z0(IJ), ALPHAP(IJ), XMSS(IJ), TAUUNR(IJ))
+          ZB(IJ) = MAX(Z0(IJ)*SQRT((TAUV+TAUUNR(IJ))/TAUOLD), Z0MINDYN)
+!         TOTAL kinematic STRESS:
           TAUNEW = US2TOTAUW*TAUW(IJ) + TAUV + TAUUNR(IJ)
           USTNEW = SQRT(TAUNEW)
           USTAR(IJ) = W1*USTOLD+(1.0_JWRB-W1)*USTNEW 
@@ -146,8 +146,7 @@ IF (LLGCBZ0) THEN
         Z0(IJ)  = MAX(XNLEV/(EXP(XKUTOP/USTAR(IJ))-1.0_JWRB),Z0MINDYN)
 
 !!!!debile
-!!          Z0VIS = RNUM/USTAR(IJ)
-          Z0VIS = 0.04_JWRB*RNU/USTAR(IJ)
+        Z0VIS = RNUM/USTAR(IJ)
        write(IU06,*) 'debile ',USTAR(IJ), Z0(IJ), ZB(IJ), Z0VIS, G*(Z0(IJ)-Z0VIS)/USTAR(IJ)**2, G*ZB(IJ)/USTAR(IJ)**2,ITER 
        write(IU06,*) 'stress ',TAUNEW, TAUW(IJ) , TAUV , TAUUNR(IJ)
        CALL FLUSH(IU06)
