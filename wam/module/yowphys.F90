4f92b8f1e216cd9a449220489e75e32a88473864
Jean Bidlot <jean.bidlot@ecmwf.int>
2020-08-16 13:38:12 +0100

    CON to 3000 !

--- wam/module/yowphys.F90
+++ wam/module/yowphys.F90
@@ -48,7 +48,6 @@
 
 !     Negative wind input, ARDHUIN et al. 2010:
       REAL(KIND=JWRB), PARAMETER :: SWELLF = 0.66_JWRB ! controls the turbulent swell dissipation
-
       REAL(KIND=JWRB), PARAMETER :: SWELLF2 = -0.018_JWRB
       REAL(KIND=JWRB), PARAMETER :: SWELLF3 = 0.022_JWRB
       REAL(KIND=JWRB), PARAMETER :: SWELLF4 = 1.5E05_JWRB
