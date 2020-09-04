0b2c9b74ce578d7a9d427fd2459fcc434a55cfcb
Jean Bidlot <jean.bidlot@ecmwf.int>
2020-09-04 09:07:23 +0100

    try gc static with limit of tauw in stresso

--- wam/Wam_oper/taut_z0.F90
+++ wam/Wam_oper/taut_z0.F90
@@ -126,16 +126,16 @@ IF (LLGCBZ0) THEN
       ENDDO
 
 !! needed if static test
-!      IF(LLCAPCHNK) THEN
-!        DO IJ=IJS,IJL
-!          CHARNOCK_MIN = CHNKMIN(UTOP(IJ))
-!          ALPHAOG(IJ) = CHARNOCK_MIN*GM1
-!        ENDDO
-!      ELSE
-!        DO IJ=IJS,IJL
-!          ALPHAOG(IJ)= ALPHA*GM1
-!        ENDDO
-!      ENDIF
+      IF(LLCAPCHNK) THEN
+        DO IJ=IJS,IJL
+          CHARNOCK_MIN = CHNKMIN(UTOP(IJ))
+          ALPHAOG(IJ) = CHARNOCK_MIN*GM1
+        ENDDO
+      ELSE
+        DO IJ=IJS,IJL
+          ALPHAOG(IJ)= ALPHA*GM1
+        ENDDO
+      ENDIF
 !!
 
       DO IJ = IJS, IJL
@@ -152,8 +152,8 @@ IF (LLGCBZ0) THEN
 
           CALL STRESS_GC(ANG_GC, USTAR(IJ), Z0(IJ), HALP(IJ), TAUUNR(IJ))
 !!! static test (need the lines above for the calculation of ALPHAOG
-!!          ZB(IJ) = ALPHAOG(IJ)*TAUOLD
-!!          TAUUNR(IJ) = (ZB(IJ)/Z0(IJ))**2*TAUOLD
+          ZB(IJ) = ALPHAOG(IJ)*TAUOLD
+          TAUUNR(IJ) = (ZB(IJ)/Z0(IJ))**2*TAUOLD
 !!
 !! ZB is diagnostic, so could be removed when not needed
 !!          ZB(IJ) = MAX(Z0(IJ)*SQRT(TAUUNR(IJ)/TAUOLD), Z0MIN)
