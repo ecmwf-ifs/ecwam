1becba6a0a09d8875a6e172b96c151561cfd995d
Jean Bidlot <jean.bidlot@ecmwf.int>
2020-10-23 14:35:29 +0100

    renormalistion in tau_phi

--- wam/Wam_oper/init_x0tauhf.F90
+++ wam/Wam_oper/init_x0tauhf.F90
@@ -32,7 +32,7 @@
 
       USE YOWCOUP  , ONLY : X0TAUHF, JTOT_TAUHF, WTAUHF,LLCAPCHNK, LLGCBZ0
       USE YOWPHYS  , ONLY : BETAMAX  ,ZALP     ,ALPHA, ALPHAMIN, XKAPPA, &
-     &                      BETAMAXOXKAPPA2, GAMNCONST
+     &                      BETAMAXOXKAPPA2, GAMNCONST, DELTA_THETA_RN
       USE YOWPCONS , ONLY : GM1      ,ZPI 
       USE YOMHOOK  , ONLY : LHOOK,   DR_HOOK
 
@@ -42,8 +42,6 @@
 
       INTEGER(KIND=JWIM) :: J
 
-      REAL(KIND=JWRB), PARAMETER :: DELTA_THETA = 0.53_JWRB  ! directionality correction factor in the 
-                                                             ! gowth rate renormalisation (see Janssen tbd)
       REAL(KIND=JWRB) :: CONST1, X0, FF, F, DF, ALPH
       REAL(KIND=JWRB) :: ZHOOK_HANDLE
 
@@ -53,7 +51,7 @@
 
       BETAMAXOXKAPPA2 = BETAMAX / XKAPPA**2
 
-      GAMNCONST = DELTA_THETA*0.5_JWRB*ZPI**4*GM1*3*BETAMAXOXKAPPA2/XKAPPA
+      GAMNCONST = DELTA_THETA_RN*0.5_JWRB*ZPI**4*GM1*3*BETAMAXOXKAPPA2/XKAPPA
 
 !*    1. PRELIMINARY CALCULATIONS.
 !        -------------------------
