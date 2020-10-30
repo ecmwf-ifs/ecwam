3f7f44e06732821a478bb529c803d90fba76a4f2
Jean Bidlot <jean.bidlot@ecmwf.int>
2020-10-30 15:58:31 +0000

    re-normalisation in gc model

--- wam/Wam_oper/stress_gc.F90
+++ wam/Wam_oper/stress_gc.F90
@@ -15,9 +15,10 @@
 
       USE PARKIND_WAVE, ONLY : JWIM, JWRB, JWRU
 
+      USE YOWCOUP  , ONLY : LLNORMAGAM
       USE YOWFRED  , ONLY : NWAV_GC, OMEGA_GC, XK_GC, &
-     &                      OMXKM3_GC, CM_GC, C2OSQRTVG_GC, OM3GMKM_GC, &
-     &                      DELKCC_GC, DELKCC_GC_NS
+     &                      OMXKM3_GC, CM_GC, C2OSQRTVG_GC, XKMSQRTVGOC2_GC, &
+     &                      OM3GMKM_GC, DELKCC_GC, DELKCC_GC_NS
       USE YOWPCONS , ONLY : G,      SURFT
       USE YOWPHYS  , ONLY : XKAPPA, ZALP,   BETAMAXOXKAPPA2
 
@@ -40,8 +41,10 @@
       INTEGER(KIND=JWIM) :: I
 
       REAL, PARAMETER :: TAUWCG_MIN = 0.00001_JWRB
-      REAL(KIND=JWRB) :: XKS, OMS
+      REAL(KIND=JWRB) :: XKS, OMS, ZABHRC
       REAL(KIND=JWRB) :: X, XLOG, ZLOG, ZLOG2X
+      REAL(KIND=JWRB) :: CONST 
+      REAL(KIND=JWRB) :: GAMNORMA ! RENORMALISATION FACTOR OF THE GROWTH RATE
       REAL(KIND=JWRB) :: ZHOOK_HANDLE
       REAL(KIND=JWRB), DIMENSION(NWAV_GC) :: GAM_W
    
@@ -58,6 +61,13 @@
 !     FIND NS:
       CALL OMEGAGC(USTAR, NS, XKS, OMS)
 
+      ZABHRC = ANG_GC * BETAMAXOXKAPPA2 * HALP * ZBREDUC * C2OSQRTVG_GC(NS)
+      IF(LLNORMAGAM) THEN
+        CONST = ZABHRC/XKAPPA
+      ELSE
+        CONST = 0.0_JWRB
+      ENDIF
+
       DO I = NS, NWAV_GC
 !       GROWTHRATE BY WIND WITHOUT the multiplicative representing the ratio of air density to water density (eps)
 !       and BETAMAXOXKAPPA2
@@ -68,7 +78,14 @@
         GAM_W(I)= ZLOG2X*EXP(ZLOG)*ZLOG2X*OM3GMKM_GC(I)
       ENDDO
 
-      TAUWCG = GAM_W(NS) * DELKCC_GC_NS(NS) * OMXKM3_GC(NS) 
+
+      GAMNORMA  = 1.0_JWRB / (1.0_JWRB + CONST*XKMSQRTVGOC2_GC(NS)*GAM_W(NS)/USTAR )
+!!!debile
+         write(*,*) 'stress_gc ',NS, GAMNORMA, USTAR, CONST, XKMSQRTVGOC2_GC(NS), GAM_W(NS)
+         GAMNORMA = 1.0_JWRB
+!!!!
+
+      TAUWCG = GAM_W(NS) * DELKCC_GC_NS(NS) * OMXKM3_GC(NS) * GAMNORMA
       DO I = NS+1, NWAV_GC
 !       ANALYTICAL FORM INERTIAL SUB RANGE F(k) = k**(-4)*BB
 !       BB = HALP * C2OSQRTVG_GC(NS)*SQRT(VG_GC(I))/C_GC(I)**2
@@ -79,9 +96,16 @@
 !       Tauwcg : integral of omega * gammma_wam * F(k)  k dk
 !       It should be done in vector form with actual directional spreading information
 !       It simplified here by using the ANG_GC factor.
-        TAUWCG = TAUWCG + GAM_W(I) * DELKCC_GC(I) * OMXKM3_GC(I) 
+
+        GAMNORMA  = 1.0_JWRB / (1.0_JWRB + CONST*XKMSQRTVGOC2_GC(I)*GAM_W(I)/USTAR )
+!!!debile
+         write(*,*) 'stress_gc ',J, GAMNORMA, USTAR, CONST, XKMSQRTVGOC2_GC(I), GAM_W(I)
+         GAMNORMA = 1.0_JWRB
+!!!!
+
+        TAUWCG = TAUWCG + GAM_W(I) * DELKCC_GC(I) * OMXKM3_GC(I) * GAMNORMA
       ENDDO
-      TAUWCG = MAX(ANG_GC * BETAMAXOXKAPPA2 * HALP * ZBREDUC * C2OSQRTVG_GC(NS) * TAUWCG, TAUWCG_MIN)
+      TAUWCG = MAX(ZABHRC * TAUWCG, TAUWCG_MIN)
 
       IF (LHOOK) CALL DR_HOOK('STRESS_GC',1,ZHOOK_HANDLE)
  
