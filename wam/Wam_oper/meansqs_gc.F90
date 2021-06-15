af7d63a40062a1020782473c99173e4b30fb6bdc
Jean Bidlot <jean.bidlot@ecmwf.int>
2021-06-15 09:57:57 +0100

    go back to using USTAR

--- wam/Wam_oper/meansqs_gc.F90
+++ wam/Wam_oper/meansqs_gc.F90
@@ -68,7 +68,7 @@
 !         mss :  integral of k**2 F(k)  k dk
           XMSSCG(IJ) = XMSSCG(IJ) + DELKCC_GC(I) * XKM_GC(I) 
         ENDDO
-        COEF = C2OSQRTVG_GC(NS(IJ))*HALPHAP(IJ)*DIRSPRD_GC(U10(IJ))
+        COEF = C2OSQRTVG_GC(NS(IJ))*HALPHAP(IJ)*DIRSPRD_GC(USTAR(IJ))
         XMSSCG(IJ) = XMSSCG(IJ)*COEF
       ENDDO
 
