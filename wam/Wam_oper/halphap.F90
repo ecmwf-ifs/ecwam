6199bed027b6bf4b6a68a32759653c5928728f39
Jean Bidlot <jean.bidlot@ecmwf.int>
2021-07-26 18:30:02 +0100

    alphap on low frequency mss only

--- wam/Wam_oper/halphap.F90
+++ wam/Wam_oper/halphap.F90
@@ -2,7 +2,7 @@ SUBROUTINE HALPHAP(IJS, IJL, USTAR, UDIR, FL1, HALP)
 
 ! ----------------------------------------------------------------------
 
-!**** *HALPHAP* - COMPUTATION OF 1/2 PHILLIPS PARAMETER in the wind direction only
+!**** *HALPHAP* - COMPUTATION OF 1/2 PHILLIPS PARAMETER
 
 
 !**   INTERFACE.
@@ -23,11 +23,10 @@ SUBROUTINE HALPHAP(IJS, IJL, USTAR, UDIR, FL1, HALP)
 
       USE PARKIND_WAVE, ONLY : JWIM, JWRB, JWRU
 
-      USE YOWFRED  , ONLY : TH       , FR       , FR5      ,FRATIO  ,FRIC     ,OLDWSFC
-      USE YOWPARAM , ONLY : NANG     ,NFRE
-      USE YOWPCONS , ONLY : G        , GM1      ,ZPI       ,ZPI4GM2,EPSMIN
+      USE YOWFRED  , ONLY : FR       , FR5      ,DELTH
+      USE YOWPARAM , ONLY : NANG     , NFRE
+      USE YOWPCONS , ONLY : G        , ZPI      ,ZPI4GM2
       USE YOWPHYS  , ONLY : ALPHAPMAX
-      USE YOWSHAL  , ONLY : TFAK     ,INDEP    ,CINV
       USE YOMHOOK  , ONLY : LHOOK,   DR_HOOK
 
 ! ----------------------------------------------------------------------
@@ -35,112 +34,45 @@ SUBROUTINE HALPHAP(IJS, IJL, USTAR, UDIR, FL1, HALP)
       IMPLICIT NONE
 #include "femean.intfb.h"
 #include "meansqs_lf.intfb.h"
-#include "peakfri.intfb.h"
 
       INTEGER(KIND=JWIM), INTENT(IN) :: IJS, IJL
+!!! debile: it it works remove use of ustar and udir
       REAL(KIND=JWRB), DIMENSION(IJS:IJL), INTENT(IN) :: USTAR
       REAL(KIND=JWRB), DIMENSION(IJS:IJL), INTENT(IN) :: UDIR
       REAL(KIND=JWRB), DIMENSION(IJS:IJL,NANG,NFRE), INTENT(IN) :: FL1
       REAL(KIND=JWRB), DIMENSION(IJS:IJL), INTENT(OUT) :: HALP
 
-      INTEGER(KIND=JWIM) :: IJ, K, M
+      INTEGER(KIND=JWIM) :: IJ, K
 
-      ! FREQUENCY INDEXES FOR THE PHILLIPS RANGE 
-      INTEGER(KIND=JWIM), PARAMETER :: IPHS = 1 + INT(LOG(1.3_JWRB)/LOG(FRATIO))
-      INTEGER(KIND=JWIM), PARAMETER :: IPHE = 1 + INT(LOG(3.0_JWRB)/LOG(FRATIO))
-
-      INTEGER(KIND=JWIM) :: MS, MM, ME
-      INTEGER(KIND=JWIM), DIMENSION(IJS:IJL) :: MMAX
-
-      REAL(KIND=JWRB), PARAMETER :: ALPHAPMAX_LW = 0.025_JWRB
-      REAL(KIND=JWRB) :: RF, WFR, CMSSHF
-      REAL(KIND=JWRB) :: COEF, WS, CHECKTA
+      REAL(KIND=JWRB) :: F1D
       REAL(KIND=JWRB) :: ZHOOK_HANDLE
-      REAL(KIND=JWRB), DIMENSION(NFRE+IPHE) :: DFRE, F5DFRE 
-      REAL(KIND=JWRB), DIMENSION(IJS:IJL) :: F1DMAX
       REAL(KIND=JWRB), DIMENSION(IJS:IJL) :: ALPHAP
       REAL(KIND=JWRB), DIMENSION(IJS:IJL) :: XMSS, EM, FM 
-      REAL(KIND=JWRB), DIMENSION(IJS:IJL,NFRE) :: XINVWVAGE
-      REAL(KIND=JWRB), DIMENSION(IJS:IJL,NANG) :: DIRCOEF
-      REAL(KIND=JWRB), DIMENSION(IJS:IJL,NFRE) :: F1DWS
-      REAL(KIND=JWRB), DIMENSION(IJS:IJL,NANG,NFRE) :: FLWS
 
 ! ----------------------------------------------------------------------
 
 IF (LHOOK) CALL DR_HOOK('HALPHAP',0,ZHOOK_HANDLE)
 
-      RF = 0.5_JWRB*(FRATIO - 1.0_JWRB/FRATIO)
-      DO M = 1, NFRE
-         DFRE(M) = FR(M)*RF
-         F5DFRE(M) = FR5(M)*DFRE(M)
-      ENDDO
-      DO M = NFRE+1, NFRE+IPHE
-         DFRE(M) = FR(NFRE)*FRATIO**(M-NFRE)*RF
-         F5DFRE(M) = FR5(NFRE)*DFRE(M)
-      ENDDO
+      CALL MEANSQS_LF(NFRE, IJS, IJL, FL1, XMSS)
+      CALL FEMEAN (FL1, IJS, IJL, EM, FM)
 
-      CMSSHF = ZPI4GM2 * FR5(NFRE) 
-
-!     Find windsea spectrum
-      COEF = OLDWSFC*FRIC
-      DO M = 1, NFRE
-        DO IJ = IJS, IJL
-          XINVWVAGE(IJ,M)=USTAR(IJ)*CINV(INDEP(IJ),M)
-        ENDDO
-      ENDDO
-
-      DO K = 1, NANG
-        DO IJ = IJS, IJL
-          DIRCOEF(IJ,K)=COEF*COS(TH(K)-UDIR(IJ))
-        ENDDO
-      ENDDO
-
-      MMAX(:) = NFRE
-      DO M = 1, NFRE
-        DO K = 1, NANG
-          DO IJ = IJS, IJL
-            CHECKTA=XINVWVAGE(IJ,M)*DIRCOEF(IJ,K)
-            WS = 0.5_JWRB + SIGN(0.5_JWRB, (CHECKTA-1.0_JWRB) )
-            FLWS(IJ,K,M) = WS*FL1(IJ,K,M)
-          ENDDO
-        ENDDO
-      ENDDO
-
-      ! Find peak of windsea 1d spectrum
-      CALL PEAKFRI (FLWS, IJS, IJL, MMAX, F1DMAX, F1DWS)
-
-      ! Find the Phillips parameter by weighting averaging its value over the Phillips range (see above)
-      ! if it is inside the discretised frequency range, otherwise use its relation to the mean square slope
       DO IJ = IJS, IJL
-        ALPHAP(IJ) = 0.0_JWRB
-        MS = MIN(MMAX(IJ) + IPHS, NFRE)
-        MM = MIN(MMAX(IJ) + IPHE, NFRE) 
-        ME = MMAX(IJ) + IPHE 
-
-        IF ( ME <= NFRE ) THEN
-          ! the Phillips range is fully resolved
-          WFR = 0.0_JWRB
-          DO M = MS, MM 
-            WFR = WFR + DFRE(M)
-            ALPHAP(IJ) = ALPHAP(IJ) + F5DFRE(M)*F1DWS(IJ,M)
-          ENDDO
-          ALPHAP(IJ) = ZPI4GM2*ALPHAP(IJ) / WFR
-        ELSE
-          ! use the mss and the mean frequency instead
-          CALL MEANSQS_LF(NFRE, IJ, IJ, FL1(IJ:IJ,:,:), XMSS(IJ))
-          ! add contribution to MSS from FR(NFRE) to FR(ME) assuming a f**-4 spectrum
-          XMSS(IJ) = XMSS(IJ) + CMSSHF * F1DWS(IJ,NFRE) * (FRATIO**MAX(ME-NFRE,0) -1.0_JWRB) 
-          CALL FEMEAN (FL1(IJ:IJ,:,:), IJ, IJ, EM(IJ), FM(IJ))
-          FM(IJ)= MIN(FM(IJ),FR(MMAX(IJ)))
-          IF(EM(IJ) > 0.0_JWRB .AND. FM(IJ) < FR(NFRE-2) ) THEN
-            ALPHAP(IJ) = XMSS(IJ) /LOG(FR(NFRE)/FM(IJ))
-            IF ( ALPHAP(IJ) > ALPHAPMAX_LW ) THEN
-              ! some odd cases, revert to tail value
-              ALPHAP(IJ) = MIN(ZPI4GM2*FR5(NFRE)*F1DWS(IJ,NFRE),ALPHAPMAX_LW)  
-            ENDIF
-          ELSE
-            ALPHAP(IJ) = MIN(ZPI4GM2*FR5(NFRE)*F1DWS(IJ,NFRE),ALPHAPMAX_LW)
+        IF(EM(IJ) > 0.0_JWRB .AND. FM(IJ) < FR(NFRE-2) ) THEN
+          ALPHAP(IJ) = XMSS(IJ) /LOG(FR(NFRE)/FM(IJ))
+          IF ( ALPHAP(IJ) > ALPHAPMAX ) THEN
+            ! some odd cases, revert to tail value
+            F1D = 0.0_JWRB
+            DO K = 1, NANG
+              F1D = F1D + FL1(IJ,K,NFRE)*DELTH
+            ENDDO
+            ALPHAP(IJ) = ZPI4GM2*FR5(NFRE)*F1D
           ENDIF
+        ELSE
+          F1D = 0.0_JWRB
+          DO K = 1, NANG
+            F1D = F1D + FL1(IJ,K,NFRE)*DELTH
+          ENDDO
+          ALPHAP(IJ) = ZPI4GM2*FR5(NFRE)*F1D
         ENDIF
       ENDDO
 
