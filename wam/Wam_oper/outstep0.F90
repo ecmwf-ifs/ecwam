c3965a99cc6d9c00204e618f1da67438e73cc494
Jean Bidlot <jean.bidlot@ecmwf.int>
2021-11-08 23:43:58 +0000

    save

--- wam/Wam_oper/outstep0.F90
+++ wam/Wam_oper/outstep0.F90
@@ -92,6 +92,19 @@ SUBROUTINE WAMODEL (NADV, LDSTOP, LDWRRE, BLK2GLO,             &
 !                                      & CALL OUTBETA MOVED TO WAVEMDL
 !
 
+!SHALLOW
+!          DIFFERENCES FOR SHALLOW WATER RUNS TO DEEP WATER RUNS
+!          ARE ENCLOSED IN COMMENT LINES : 'CSHALLOW'.
+!SHALLOW
+!NEST
+!          DIFFERENCES FOR NESTED GRID RUNS TO NORMAL RUNS
+!          ARE ENCLOSED IN COMMENT LINES : 'CNEST'.
+!NEST
+!REFRA
+!          DIFFERENCES FOR REFRACTION RUNS TO NORMAL RUNS
+!          ARE ENCLOSED IN COMMENT LINES : 'CREFRA'.
+!REFRA
+
 !*    PURPOSE.
 !     --------
 
@@ -194,6 +207,8 @@ SUBROUTINE WAMODEL (NADV, LDSTOP, LDWRRE, BLK2GLO,             &
       USE YOWWIND  , ONLY : CDATEWO
       USE UNWAM, ONLY : EXCHANGE_FOR_FL1
       USE YOWUNPOOL ,ONLY : LLUNSTR, LLUNBINOUT
+      USE YOWPD, ONLY : MNP => npa
+      USE YOWNODEPOOL, ONLY : NP
 
       USE MPL_MODULE
       USE FDBSUBS_MOD, ONLY : IFLUSHFDBSUBS
@@ -217,7 +232,6 @@ SUBROUTINE WAMODEL (NADV, LDSTOP, LDWRRE, BLK2GLO,             &
 #include "outbs.intfb.h"
 #include "outint.intfb.h"
 #include "outspec.intfb.h"
-#include "outstep0.intfb.h"
 #include "savspec.intfb.h"
 #include "savstress.intfb.h"
 #include "setice.intfb.h"
@@ -275,9 +289,6 @@ ASSOCIATE(WSWAVE => FF_NOW%WSWAVE, &
  &        STRNMS  => INTFLDS%STRNMS )
 
 
-!     0.0 INITIALISATION
-!         --------------
-
       CZERO = ' '
       LLFLUSH = .FALSE.
       KTAG=200
@@ -290,8 +301,9 @@ ASSOCIATE(WSWAVE => FF_NOW%WSWAVE, &
 !     TIME FOR WIND INPUT UPDATE (SEE NEWWIND)
       CDTIMP=CDTPRO
 
-!     0.1 MINIMUM ENERGY
-!         --------------
+!*    0. WRITE OUT NORMS FOR WAVE ENERGY, PEAK PERIOD AND WIND SPEED
+!        -----------------------------------------------------------
+
       IF (CDTPRO == CDATEA .AND. LLSOURCE ) THEN
 !       INSURE THERE IS SOME WAVE ENERGY FOR GRID POINTS THAT HAVE BEEN
 !       FREED FROM SEA ICE (ONLY DONE INITIALLY AND IF THE MODEL IS NOT
@@ -309,15 +321,132 @@ ASSOCIATE(WSWAVE => FF_NOW%WSWAVE, &
         CALL GSTATS(1236,1)
       ENDIF
 
-
-!     0.2 OUTPUT INITIAL CONDITION AND/OR FORECAST STEP 0
-!         -----------------------------------------------
       IF (CDTPRO == CDATEA .OR. CDTPRO == CDATEF) THEN
 
-debile
-call outstep0
+        CDTINTTBAK=CDTINTT
+        CDTINTT=CDTPRO
 
-      ENDIF
+        IF (LLSOURCE) THEN
+
+!$OMP     PARALLEL DO SCHEDULE(DYNAMIC,1) PRIVATE(JKGLO,KIJS,KIJL)
+          DO JKGLO=IJS,IJL,NPROMA
+            KIJS=JKGLO
+            KIJL=MIN(KIJS+NPROMA-1,IJL)
+            CALL WDFLUXES (KIJS, KIJL,                                 &
+     &                     MIJ(KIJS),                                  &
+     &                     FL1(KIJS:KIJL,:,:), XLLWS(KIJS:KIJL,:,:),   &
+     &                     WVPRPT(KIJS:KIJL,:),                        &
+     &                     WVENVI(KIJS), FF_NOW(KIJS),                 &
+     &                     INTFLDS(KIJS), WAM2NEMO(KIJS) )
+          ENDDO
+!$OMP     END PARALLEL DO
+
+          IF (LLUNSTR) CALL EXCHANGE_FOR_FL1(FL1)
+
+        ELSE
+          MIJ(:) = NFRE
+          XLLWS(:,:,:) = 0._JWRB
+        ENDIF
+
+!       SET FL1 ON ICE POINTS TO ZERO
+        IF (LICERUN .AND. LMASKICE .AND. LLSOURCE) THEN
+! Mod for OPENMP
+          CALL GSTATS(1439,0)
+!$OMP     PARALLEL DO SCHEDULE(DYNAMIC,1) PRIVATE(JKGLO,KIJS,KIJL)
+          DO JKGLO=IJS,IJL,NPROMA
+            KIJS=JKGLO
+            KIJL=MIN(KIJS+NPROMA-1,IJL)
+            CALL SETICE(KIJS, KIJL, FL1(KIJS:KIJL,:,:) ,            &
+     &                  CICOVER(KIJS:KIJL), WSWAVE(KIJS:KIJL), WDWAVE(KIJS:KIJL))
+          ENDDO
+!$OMP     END PARALLEL DO
+          CALL GSTATS(1439,1)
+        ENDIF
+
+!       OUTPUT POINT SPECTRA
+        IF (NGOUT > 0 .OR. LLOUTERS) THEN
+          CALL OUTWPSP (IJS, IJL, FL1, FF_NOW)
+        ENDIF
+
+!*      0.1 SAVE INITIAL INTEGRATED FIELDS
+!           ------------------------------
+        IF ((MARSTYPE == 'cf' .OR. MARSTYPE == 'pf' .OR.                 &
+     &       MARSTYPE == 'fc' .OR. MARSTYPE == '4v' .OR.                 &
+     &       LANAONLY         .OR. LFRSTFLD             )                &
+     &     .AND. LWAMANOUT) THEN
+
+          IF (LFRSTFLD) THEN
+            FFLAGBAK=FFLAG
+            GFLAGBAK=GFLAG
+            MARSTYPEBAK=MARSTYPE
+            IF (MARSTYPE == 'fg'.OR.MARSTYPE == 'an') THEN
+              IF (.NOT.LNOCDIN) THEN
+                FFLAG(IRCD)=.FALSE.
+                GFLAG(IRCD)=.FALSE.
+              ENDIF
+              IF (LWAVEWIND) THEN
+                FFLAG(IRU10)=.FALSE.
+                GFLAG(IRU10)=.FALSE.
+              ENDIF
+              GFLAG(IRALTHS)=.FALSE.
+              GFLAG(IRALTHSC)=.FALSE.
+              GFLAG(IRALTRC)=.FALSE.
+              MARSTYPE='an'
+            ENDIF
+!           change the output flags need to reset mapping
+            CALL MPCRTBL
+          ENDIF
+!         NEED TO TEMPORARLY RESET THE IFS FORECAST STEP
+!         IF THE IFS GRIB HEADER IS USED SUCH THAT IT POINTS TO
+!         THE START OF THE RUN.
+          IF (LGRHDIFS) THEN
+             LRSTST0=.TRUE.
+          ENDIF
+
+!         COMPUTE OUTPUT PARAMETERS AND PRINT OUT NORMS
+          IF (NIPRMOUT > 0) THEN
+            CALL OUTBS (IJS, IJL, MIJ, FL1, XLLWS,                    &
+     &                  WVPRPT, WVENVI, FF_NOW, INTFLDS, NEMO2WAM)
+          ENDIF
+
+          IF ( .NOT. LRESTARTED ) THEN
+            IF (IREST == 1 .AND. MARSTYPE /= 'an' .AND. LGRIBOUT) THEN
+              CALL OUTSPEC(IJS, IJL, FL1, CICOVER)
+              LLFLUSH = .TRUE.
+            ENDIF
+
+            IF (NIPRMOUT > 0 ) THEN
+              CALL OUTWINT
+              LLFLUSH = .TRUE.
+            ENDIF
+
+          ENDIF
+
+          IF (LFDB .AND. LLFLUSH) THEN
+             CALL GSTATS(1976,0)
+             CALL IFLUSHFDBSUBS()
+             CALL GSTATS(1976,1)
+             LLFLUSH=.FALSE.
+          ENDIF
+
+          IF (LFRSTFLD) THEN
+            MARSTYPE=MARSTYPEBAK
+            FFLAG=FFLAGBAK
+            GFLAG=GFLAGBAK
+            LFRSTFLD=.FALSE.
+!           change the output flags need to reset mapping
+            CALL MPCRTBL
+          ENDIF
+
+          CDTINTT=CDTINTTBAK
+
+          IF (LGRHDIFS) THEN
+            LRSTST0=.FALSE.
+          ENDIF
+
+        ENDIF
+
+      ENDIF ! DO_UPDATE_WIND_FLUX
 
 
 
@@ -327,13 +456,14 @@ call outstep0
 
       ADVECTION : DO KADV = 1,NADV
 
-!*      1.1 FIX END DATE OF THIS PROPAGATION STEP AND OUTPUT TIMES.
-!           -------------------------------------------------------
+!*    1.1 FIX END DATE OF THIS PROPAGATION STEP AND OUTPUT TIMES.
+!         -------------------------------------------------------
 
         CDTPRA = CDTPRO
         CALL INCDATE(CDTPRO,IDELPRO)
 
-!       UPDATE OUTPUT TIMES.
+!         UPDATE OUTPUT TIMES.
+
         IF (NOUTT > 0) THEN
           CDTINTT = CZERO
           DO J=1,NOUTT
@@ -362,18 +492,15 @@ call outstep0
           IF (CDTRES < CDTPRO) CALL INCDATE(CDTRES,IDELRES)
         ENDIF
 
-!NEST (not used at ECMWF)
         IF ((IBOUNC == 1 .OR. IBOUNF == 1) .AND. CDTBC < CDTPRO) CALL INCDATE(CDTBC,IDELBC)
-!NEST
 
 
 
-!            -------------------------------------------------------
-!*      1.2  THIS IS THE CORE OF THE WAVE MODEL:
-!*           COMPUTATION OF PROPAGATION
-!*           INTEGRATION OF SOURCE TERMS OVER SUB TIME STEPS BETWEEN
-!*           PROPAGATION TIME STEPS.
-!            -------------------------------------------------------
+!*       THIS IS THE CORE OF THE WAVE MODEL:
+!*       COMPUTATION OF PROPAGATION
+!*       INTEGRATION OF SOURCE TERMS OVER SUB TIME STEPS BETWEEN
+!*       PROPAGATION TIME STEPS.
+!        -------------------------------------------------------
 !        SET TIME COUNTER.
          CDATE   = CDTPRA
          CDATEWH = CDATEWO
@@ -388,64 +515,69 @@ call outstep0
 
 
 #ifdef ECMWF
-        IF (.NOT.LWCOU .AND. .NOT. LDSTOP) THEN
+          IF (.NOT.LWCOU .AND. .NOT. LDSTOP) THEN
 !!!!      the call to CHESIG is a signal handeling facility which is
 !!!!      specific to running WAM at ECMWF, it can be ignored when
 !!!!      WAM is not run at ECMWF.
             CALL CHESIG (IU06, ITEST, IRANK, NPROC, LDSTOP, LDWRRE)
-        ENDIF
-#endif
-
-!       1.3 CHECK WHETHER OUTPUT(s) NEEDED
-!           ------------------------------
-        LRST=(LDWRRE .AND. KADV == NADV )
-        IF (LRST) THEN
-          WRITE(IU06,*) ' '
-          WRITE(IU06,*) '  ******************************************'
-          IF (LDSTOP) THEN
-          WRITE(IU06,*) '  AN INTERRUPT SIGNAL HAS BEEN RECEIVED '
           ENDIF
-          WRITE(IU06,*) '  THE NECESSARY BINARY RESTART FILES WILL BE'
-          WRITE(IU06,*) '  GENERATED.'
-          WRITE(IU06,*) '  ******************************************'
-          WRITE(IU06,*) ' '
-          CALL FLUSH (IU06)
-        ENDIF
+#endif
+          CALL GSTATS(1909,0)
+          LRST=(LDWRRE .AND. KADV == NADV )
+          IF (LRST) THEN
+            WRITE(IU06,*) ' '
+            WRITE(IU06,*) '  ******************************************'
+            IF (LDSTOP) THEN
+            WRITE(IU06,*) '  AN INTERRUPT SIGNAL HAS BEEN RECEIVED '
+            ENDIF
+            WRITE(IU06,*) '  THE NECESSARY BINARY RESTART FILES WILL BE'
+            WRITE(IU06,*) '  GENERATED.'
+            WRITE(IU06,*) '  ******************************************'
+            WRITE(IU06,*) ' '
+            CALL FLUSH (IU06)
+          ENDIF
+          CALL GSTATS(1909,1)
 
 
 !NEST (not used at ECMWF)
-!*      1.4.1 INPUT OF BOUNDARY VALUES.
+!*    1.5.7 INPUT OF BOUNDARY VALUES.
 !           -------------------------
-        IF (IBOUNF == 1) CALL BOUINPT (IU02, FL1, IJS, IJL, NBLKS, NBLKE)
-!*      1.4.2 OUTPUT OF BOUNDARY POINTS.
+          IF (IBOUNF == 1) THEN
+            CALL BOUINPT (IU02, FL1, IJS, IJL, NBLKS, NBLKE)
+          ENDIF
+!*    1.5.9 OUTPUT OF BOUNDARY POINTS.
 !           --------------------------
-        IF (IBOUNC == 1) CALL OUTBC (FL1, IJS, IJL, IU19) 
+          IF (IBOUNC == 1) THEN
+            CALL OUTBC (FL1, IJS, IJL, IU19)
+          ENDIF
 !NEST
 
 
-!*      1.5 POINT OUTPUT (not usually used at ECMWF) 
-!           ----------------------------------------
-        IF ( NGOUT > 0 .AND. (CDTINTT == CDTPRO .OR. LRST) ) THEN
+!*    1.5.10 MODEL OUTPUT INTEGRATED DATA ARE SAVED
+!            --------------------------------------
+          IF ( CDTINTT == CDTPRO .OR. LRST ) THEN
 !           OUTPUT POINT SPECTRA (not usually used at ECMWF)
-            IF (LLOUTERS) CALL OUTWPSP (IJS, IJL, FL1, FF_NOW)
-        ENDIF
+            IF (NGOUT > 0 .OR. LLOUTERS) THEN
+              CALL OUTWPSP (IJS, IJL, FL1, FF_NOW)
+            ENDIF
+
+!           COMPUTE OUTPUT PARAMETERS AND PRINT OUT NORMS
+            IF (NIPRMOUT > 0) THEN
+              CALL OUTBS (IJS, IJL, MIJ, FL1, XLLWS,                   &
+     &                    WVPRPT, WVENVI, FF_NOW, INTFLDS, NEMO2WAM)
+            ENDIF
+
+          ENDIF
 
 
-!       1.6 COMPUTE OUTPUT PARAMETERS FIELDS AND PRINT OUT NORMS
-!           ----------------------------------------------------
-        IF ( (CDTINTT == CDTPRO .OR. LRST) .AND. NIPRMOUT > 0 ) THEN
-            CALL OUTBS (IJS, IJL, MIJ, FL1, XLLWS,                   &
-     &                  WVPRPT, WVENVI, FF_NOW, INTFLDS, NEMO2WAM)
-        ENDIF
+!*    1.7 ONE PROPAGATION TIMESTEP DONE
+!         -----------------------------
 
-
-!*      1.7 ONE PROPAGATION TIMESTEP DONE
-!           -----------------------------
         WRITE(IU06,*) ' !!!!!!!!!!!!!! WAVE FIELDS INTEGRATED FOR DATE : ', CDTPRO
 
-!       IS THIS AN ANALYSIS TIME FROM THE INPUT LIST ?
         LLNONASSI=.TRUE.
         IF (IASSI == 1) THEN
+!         IS THIS AN ANALYSIS TIME FROM THE INPUT LIST ?
           DO J=1,NASS
             IF (CDTPRO == CASS(J)) THEN
               LLNONASSI=.FALSE.
@@ -455,24 +587,23 @@ call outstep0
         ENDIF
 
 
-!*      1.8 SAVE FIELD OF 2D SPECTRA AND/OR BINARY RESTART FILES.
-!           -----------------------------------------------------
+!*      SAVE 2D SPECTRA AND/OR RESTART FIELDS.
+!       --------------------------------------
+        LSV=(CDTRES == CDTPRO .OR. CDATEE == CDTPRO .OR. CDTPRO == CDATER)
+
+        IF (LSV .OR. LRST) THEN
+
 !         THIS WILL HAPPEN WHEN IT IS NOT IN DATA ASSIMILATION MODE AND
 !         IT IS EITHER A DETERMINED OUTPUT TIME
 !         OR THE INTERUPT SIGNAL HAS BEEN TRIGGERED and it will wait
 !         until the end of the advection loop.
 !         OTHERWISE THE OUTPUT WILL OCCUR IN WAMASSI.
 
-        LSV=(CDTRES == CDTPRO .OR. CDATEE == CDTPRO .OR. CDTPRO == CDATER)
-
-        IF (LSV .OR. LRST) THEN
-
-          LOUT = ((IREST == 1) .AND. (CDTPRO == CDATER .OR. CDTPRO <= CDATES)) .AND. LSV .AND. LWAMANOUT
+          LOUT= ((IREST.EQ.1) .AND. (CDTPRO.EQ.CDATER .OR. CDTPRO.LE.CDATES)) .AND. LSV .AND. LWAMANOUT
 
           IF ( LOUT .OR. LRST ) THEN
 
-!           1.8.1 SAVE SPECTRUM IN GRIB
-!                 ---------------------
+!           SAVE SPECTRUM IN GRIB
             IF (LOUT .AND. LGRIBOUT) THEN
 !             we have insured that the spectra will be written to FDB
 !             even when the restart option is triggered and it is an
@@ -496,8 +627,7 @@ call outstep0
               WRITE(IU06,*) ' '
             ENDIF  
 
-!           1.8.2 SAVE RESTART FILES IN PURE BINARY FORM (in needed)
-!                 --------------------------------------
+!           SAVE RESTART FILES IN PURE BINARY FORM
             IF ( .NOT.LGRIBOUT .OR. LDWRRE ) THEN
               CALL SAVSTRESS(IJS, IJL, WVENVI, FF_NOW, NBLKS, NBLKE, CDTPRO, CDATEF)
               WRITE(IU06,*) ' '
@@ -511,8 +641,7 @@ call outstep0
             ENDIF
 
 
-!*          1.8.3 UPDATE, WRITE AND SAVE WAMINFO FILE.
-!                 -----------------------------------
+!*          UPDATE, WRITE AND SAVE WAMINFO FILE.
             IF (LRST .AND. IRANK == 1) THEN
               ICH = 7 
               CALL DIFDATE (CDATEF,CDATEE,IFOREPD)
@@ -548,7 +677,6 @@ call outstep0
               CALL FLUSH(IU06)
 
               IF (LRSTINFDAT) THEN
-!               WRITE AN ADDITIONAL wamfile WITH DATE/TIME INFO added to filename
                 CDTRCF=CDTPRO
                 CALL INCDATE(CDTRCF,IDELPRO)
                 IU04 =  IWAM_GET_UNIT (IU06,CWI(1:ICPLEN+8)//'.'//      &
@@ -561,15 +689,15 @@ call outstep0
                 CLOSE (IU04)
               ENDIF
 
-            ENDIF  ! waminfo
+            ENDIF
 
           ENDIF  ! END SAVE RESTART FIELDS
 
-        ENDIF  ! END SAVE 1.8
+        ENDIF  ! END SAVE
 
 
-!       1.9 WRITE INTEGRATED PARAMETER DATA TO FDB OR TO FILE
-!           -------------------------------------------------
+!       WRITE INTEGRATED DATA TO FDB OR TO FILE
+!       ---------------------------------------
         IF (LWAMANOUT .AND. CDTINTT == CDTPRO .AND. NIPRMOUT > 0 ) THEN
 !         IF THE OUTPUT TIME IS NOT AN ANALYSIS TIME THEN TYPE FG or 4V
 !         BECOMES TYPE AN (i.e. speudo analysis)
@@ -589,9 +717,8 @@ call outstep0
           CALL GSTATS(753,1)
         ENDIF
 
-
-!*      1.10 FLUSH FDB IF IT HAS BEEN USED AND IT IS NOT AN ANALYSIS (it will be done in *wamassi*)
-!            -------------------------------------------------------
+!*      FLUSH FDB IF IT HAS BEEN USED AND IT IS NOT AN ANALYSIS
+!       -------------------------------------------------------
 
         IF ( LFDB .AND. LLFLUSH .AND. (IASSI /= 1 .OR. CDTPRO > CDATEF) ) THEN
           CALL GSTATS(1976,0)
@@ -604,19 +731,21 @@ call outstep0
         ENDIF
 
 
-!       1.11 FOR PURE BINARY DATA (obsolete option at ECMWF !!!!!!!):
-!*           OUTPUT FILES AND RECOVERY FILES ARE DISPOSED WHEN
-!            TIME REACHES THE DISPOSE DATE OR WHEN THE MODEL
-!            HAS BEEN SIGNALLED TO DO SO.
-!            -------------------------------------------------
-        IF (FFLAG20) THEN
-          IF (CDATEE == CDTPRO .AND. LOUTINT .AND. IRANK == 1  ) THEN
+!*      OUTPUT FILES AND RECOVERY FILES ARE DISPOSED WHEN
+!       TIME REACHES THE DISPOSE DATE OR WHEN THE MODEL
+!       HAS BEEN SIGNALLED TO DO SO.
+!       -------------------------------------------------
+
+        IF (CDATEE == CDTPRO .AND. LOUTINT ) THEN
+!         MOVE INTEGRATED PARAMETERS OF ENTIRE GRID TO PERMANENT FILES
+!         FOR PURE BINARY DATA (obsolete option):
+          IF (FFLAG20 .AND. IRANK == 1 ) THEN
             CALL GSFILE (IU06, IU20, 0, CDTPRO, CDATEF, 'MAP', 'S')
           ENDIF
         ENDIF
 
-!NEST (not used at ECMWF)
 !       SAVE BOUNDARY VALUE FILE.
+!NEST
         IF (CDTBC == CDTPRO) THEN
           IF (IBOUNC == 1 .AND. IRANK == 1 ) THEN
             DO II=1,GBOUNC
@@ -631,7 +760,7 @@ call outstep0
 !NEST
  
 
-!*      1.12 WAM-NEMO COUPLING (!!!!! WHEN NO atmospheric model !!!!!!) (currently not used at ECMWF)
+!*      WAM-NEMO COUPLING (!!!!! WHEN NO atmospheric model !!!!!!)
 !       (when coupled see cnt4 in ifs)
 !       ----------------------------------------------------
         IF (LWNEMOCOU .AND. (.NOT.LWCOU)) THEN
@@ -651,7 +780,6 @@ call outstep0
             NEMOCSTEP=NEMOCSTEP+NEMONSTEP
           ENDIF
         ENDIF
-
 
 !*    BRANCHING BACK TO 1.0 FOR NEXT PROPAGATION STEP.
       ENDDO ADVECTION
