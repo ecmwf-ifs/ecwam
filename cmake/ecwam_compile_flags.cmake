204262ed053a9404fcf6eed0e0e0181707c0cc85
Willem Deconinck <willem.deconinck@ecmwf.int>
2023-02-07 16:06:28 +0000

    IFS-2726 Support compilation with Flang

--- cmake/ecwam_compile_flags.cmake
+++ cmake/ecwam_compile_flags.cmake
@@ -8,46 +8,59 @@
 
 if(CMAKE_Fortran_COMPILER_ID MATCHES "Cray")
   set(autopromote_flags   "-sreal64")
-  set(convertendian_flags "-hbyteswapio")
   set(checkbounds_flags   "-Rb")
   set(fpe_flags           "-Ktrap=fp")
   set(initsnan_flags      "-ei")
+
 elseif(CMAKE_Fortran_COMPILER_ID MATCHES "GNU")
   set(autopromote_flags   "-fdefault-real-8 -fdefault-double-8")
-  set(convertendian_flags "-fconvert=big-endian" )
   set(checkbounds_flags   "-fcheck=bounds")
   set(fpe_flags           "-ffpe-trap=invalid,zero,overflow")
   set(initsnan_flags      "-finit-real=snan")
-  if( NOT CMAKE_Fortran_COMPILER_VERSION VERSION_LESS 10 )
-    ecbuild_add_fortran_flags( "-fallow-argument-mismatch" NAME argument_mismatch )
-  endif()
+
 elseif(CMAKE_Fortran_COMPILER_ID MATCHES "Intel")
   set(autopromote_flags   "-real-size 64")
-  set(convertendian_flags "-convert big_endian")
   set(checkbounds_flags   "-check bounds")
   set(initsnan_flags      "-init=snan")
   set(fpe_flags           "-fpe0")
-  ecbuild_add_fortran_flags( "-check noarg_temp_created" NAME check_noarg_temp_created BUILD DEBUG )
+
 elseif(CMAKE_Fortran_COMPILER_ID MATCHES "PGI|NVHPC")
   set(autopromote_flags   "-r8")
-  set(convertendian_flags "-Mbyteswapio")
   set(checkbounds_flags   "-Mbounds")
   set(fpe_flags           "-Ktrap=fp")
+
+elseif(CMAKE_Fortran_COMPILER_ID MATCHES "Flang")
+  set(autopromote_flags   "-fdefault-real-8")
+  set(fpe_flags           "-ffp-exception-behavior=strict")
+
 endif()
 
 if( NOT HAVE_SINGLE_PRECISION )
   ecbuild_add_fortran_flags( "${autopromote_flags}"   NAME autopromote )
 endif()
-#ecbuild_add_fortran_flags( "${convertendian_flags}" NAME convert_bigendian )
 
 if( CMAKE_BUILD_TYPE MATCHES "Debug" )
-  if( fpe_flags )
-    ecbuild_add_fortran_flags( "${fpe_flags}"         NAME fpe)
+  foreach( debug_flag    fpe initsnan checkbounds )
+    if( ${debug_flag}_flags )
+      ecbuild_add_fortran_flags( "${${debug_flag}_flags}" NAME ${debug_flag} )
+    endif()
+  endforeach()
+  if(CMAKE_Fortran_COMPILER_ID MATCHES "Intel")
+    # In case '-check all' has been added, we need to remove the '-check arg_temp_created' warnings
+    ecbuild_add_fortran_flags( "-check noarg_temp_created" NAME check_noarg_temp_created BUILD DEBUG ) # the BUILD DEBUG argument makes sure it is appended after '-check all'
   endif()
-  if( initsnan_flags )
-    ecbuild_add_fortran_flags( "${initsnan_flags}"    NAME initsnan)
+endif()
+
+if(CMAKE_Fortran_COMPILER_ID MATCHES "GNU")
+  if( NOT CMAKE_Fortran_COMPILER_VERSION VERSION_LESS 10 )
+    ecbuild_add_fortran_flags( "-fallow-argument-mismatch" NAME argument_mismatch )
   endif()
-  if( checkbounds_flags )
-    ecbuild_add_fortran_flags( "${checkbounds_flags}" NAME checkbounds)
-  endif()
+endif()
+
+if(CMAKE_Fortran_COMPILER_ID MATCHES "Flang")
+  # Linker complains of unknown arguments:
+  #    warning: argument unused during compilation: '-fdefault-real-8' [-Wunused-command-line-argument]
+  foreach( LINKER_FLAGS CMAKE_EXE_LINKER_FLAGS CMAKE_SHARED_LINKER_FLAGS CMAKE_STATIC_LINKER_FLAGS )
+    set( ${LINKER_FLAGS} "${${LINKER_FLAGS}} -Wno-unused-command-line-argument")
+  endforeach()
 endif()
