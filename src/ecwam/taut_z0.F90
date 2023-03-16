d97c2c62ec66000a81c31e7699cb64998c4c6d3a
Ahmad Nawab <ahmad.nawab@ecmwf.int>
2023-03-16 08:45:49 +0100

    Removed statement function in src/ecwam/taut_z0.F90

--- src/ecwam/taut_z0.F90
+++ src/ecwam/taut_z0.F90
@@ -127,16 +127,6 @@ SUBROUTINE TAUT_Z0(KIJS, KIJL, IUSFG, FL1, WAVNUM,         &
 
 ! ----------------------------------------------------------------------
 
-!     INLINE FUNCTION.
-!     ----------------
-
-!     Simple empirical fit to model drag coefficient
-      REAL(KIND=JWRB) :: CDM, U10 
-
-      CDM(U10) = MAX(MIN(0.0006_JWRB+0.00008_JWRB*U10, 0.001_JWRB+0.0018_JWRB*EXP(-0.05_JWRB*(U10-33._JWRB))),0.001_JWRB)
-
-! ----------------------------------------------------------------------
-
 IF (LHOOK) CALL DR_HOOK('TAUT_Z0',0,ZHOOK_HANDLE)
 
       XLOGXL=LOG(XNLEV)
@@ -345,5 +335,18 @@ ELSE
 ENDIF
 
 IF (LHOOK) CALL DR_HOOK('TAUT_Z0',1,ZHOOK_HANDLE)
+
+CONTAINS
+
+!  INLINE FUNCTION.
+!  ----------------
+
+!  Simple empirical fit to model drag coefficient
+   FUNCTION CDM(U10)
+      REAL(KIND=JWRB), INTENT(IN) :: U10 
+      REAL(KIND=JWRB) :: CDM
+
+      CDM = MAX(MIN(0.0006_JWRB+0.00008_JWRB*U10, 0.001_JWRB+0.0018_JWRB*EXP(-0.05_JWRB*(U10-33._JWRB))),0.001_JWRB)
+   END FUNCTION CDM
 
 END SUBROUTINE TAUT_Z0
