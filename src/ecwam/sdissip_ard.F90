24fb2bc85085b785548bdbe62c970a0dc95caf90
Ahmad Nawab <ahmad.nawab@ecmwf.int>
2023-04-20 09:23:01 +0200

    Optimized sdissip_ard

--- src/ecwam/sdissip_ard.F90
+++ src/ecwam/sdissip_ard.F90
@@ -64,7 +64,7 @@
 
       USE YOWFRED  , ONLY : FR      , TH     ,ZPIFR
       USE YOWPCONS , ONLY : G        ,ZPI
-      USE YOWPARAM , ONLY : NANG    ,NFRE
+      USE YOWPARAM , ONLY : NANG    ,NFRE, NANG_PARAM
       USE YOWPHYS  , ONLY : SDSBR   ,ISDSDTH ,ISB     ,IPSAT    ,      &
 &                  SSDSC2  , SSDSC4, SSDSC6,  MICHE, SSDSC3, SSDSBRF1, &
 &                  BRKPBCOEF ,SSDSC5, NSDSNTH, NDIKCUMUL,              &
@@ -87,23 +87,21 @@
 
 
       INTEGER(KIND=JWIM) :: IJ, K, M, I, J, M2, K2, KK, NANGD
-      INTEGER(KIND=JWIM), DIMENSION(NANG) :: KKD
+      INTEGER(KIND=JWIM), DIMENSION(NANG_PARAM) :: KKD
 
       REAL(KIND=JWRB) :: TPIINV, TPIINVH, TMP01, TMP03
       REAL(KIND=JWRB) :: EPSR, SSDSC6M1, ZCOEF, ZCOEFM1
       REAL(KIND=JPHOOK) :: ZHOOK_HANDLE
 
 
-      REAL(KIND=JWRB), DIMENSION(NFRE) :: SIG, SSDSC2_SIG 
-      REAL(KIND=JWRB), DIMENSION(KIJS:KIJL) :: FACTURB
-      REAL(KIND=JWRB), DIMENSION(KIJS:KIJL,NFRE) :: FACSAT, FACWTRB, TEMP1 
-      REAL(KIND=JWRB), DIMENSION(KIJS:KIJL,NFRE) :: BTH0 !saturation spectrum
-      REAL(KIND=JWRB), DIMENSION(KIJS:KIJL,NANG,NFRE) :: BTH !saturation spectrum 
-      REAL(KIND=JWRB), DIMENSION(KIJS:KIJL,NANG,NFRE) :: TEMP2
+      REAL(KIND=JWRB) :: SSDSC2_SIG 
+      REAL(KIND=JWRB), DIMENSION(KIJS:KIJL) :: FACTURB, BTH, BTH0
       REAL(KIND=JWRB), DIMENSION(KIJS:KIJL,NANG,NFRE) :: D
+    
+      !$loki privatize
       REAL(KIND=JWRB), DIMENSION(KIJS:KIJL,NANG,NFRE) :: SCUMUL 
-      REAL(KIND=JWRB), DIMENSION(KIJS:KIJL,NANG,NFRE) :: RENEWALFREQ
-      REAL(KIND=JWRB), DIMENSION(KIJS:KIJL,0:NANG/2,NFRE) :: WCUMUL
+
+      REAL(KIND=JWRB), DIMENSION(KIJS:KIJL) :: RENEWALFREQ
 
 ! ----------------------------------------------------------------------
 
@@ -111,70 +109,69 @@
 
       ! INITIALISATION
 
-      EPSR=SQRT(SDSBR)
+      EPSR = SQRT(SDSBR)
 
       TPIINV = 1.0_JWRB/ZPI
       TPIINVH= 0.5_JWRB*TPIINV
-
-
       TMP03 = 1.0_JWRB/(SDSBR*MICHE)
-
       SSDSC6M1=1._JWRB-SSDSC6
 
       DO M=1,NFRE
-        SIG(M) = ZPIFR(M)
-        SSDSC2_SIG(M)=SSDSC2*SIG(M)
-      END DO
-
-      DO M=1, NFRE
-        DO IJ=KIJS,KIJL
-          FACSAT(IJ,M) = WAVNUM(IJ,M)*TPIINV*XK2CG(IJ,M)
-        ENDDO
-      ENDDO
 
       ! COMPUTE SATURATION SPECTRUM
-      DO M=1,NFRE
         DO IJ=KIJS,KIJL
-          BTH0(IJ,M) = 0.0_JWRB
+          BTH0(IJ) = 0.0_JWRB
         ENDDO
+
+        ! SATURATION TERM
+        SSDSC2_SIG = SSDSC2*ZPIFR(M)
+        ZCOEF = SSDSC2_SIG*SSDSC6
+        ZCOEFM1 = SSDSC2_SIG*SSDSC6M1
+
         DO K=1,NANG
           DO IJ=KIJS,KIJL
-            BTH(IJ,K,M) = 0.0_JWRB
+            BTH(IJ) = 0.0_JWRB
           ENDDO
-        ENDDO
-      ENDDO
 
-      DO M=1,NFRE
-        DO K=1,NANG
           ! integrates in directional sector
           DO K2=1,NSDSNTH*2+1
             KK=INDICESSAT(K,K2)
             DO IJ=KIJS,KIJL
-              BTH(IJ,K,M) = BTH(IJ,K,M) + SATWEIGHTS(K,K2)*FL1(IJ,KK,M)
+              BTH(IJ) = BTH(IJ) + SATWEIGHTS(K,K2)*FL1(IJ,KK,M)
             ENDDO
           ENDDO
           DO IJ=KIJS,KIJL
-            BTH(IJ,K,M)=BTH(IJ,K,M)*FACSAT(IJ,M)
-            BTH0(IJ,M)=MAX(BTH0(IJ,M), BTH(IJ,K,M))
+            BTH(IJ)=BTH(IJ)*WAVNUM(IJ,M)*TPIINV*XK2CG(IJ,M)
+            BTH0(IJ)=MAX(BTH0(IJ), BTH(IJ))
           ENDDO
         ENDDO
-      ENDDO
 
-
-      ! SATURATION TERM
-
-      DO  M=1,NFRE
-        ZCOEF = SSDSC2_SIG(M)*SSDSC6
-        DO IJ=KIJS,KIJL
-          TEMP1(IJ,M) = ZCOEF * (MAX(0._JWRB, BTH0(IJ,M)*TMP03-SSDSC4))**IPSAT
-        ENDDO
-      ENDDO
-      DO  M=1,NFRE
-        ZCOEFM1 = SSDSC2_SIG(M)*SSDSC6M1
         DO K=1,NANG
           DO IJ=KIJS,KIJL
-            D(IJ,K,M) = TEMP1(IJ,M) + ZCOEFM1 * (MAX(0._JWRB, BTH(IJ,K,M)*TMP03-SSDSC4))**IPSAT
+            BTH(IJ) = 0.0_JWRB
           ENDDO
+          ! integrates in directional sector
+          DO K2=1,NSDSNTH*2+1
+            KK=INDICESSAT(K,K2)
+            DO IJ=KIJS,KIJL
+              BTH(IJ) = BTH(IJ) + SATWEIGHTS(K,K2)*FL1(IJ,KK,M)
+            ENDDO
+          ENDDO
+          DO IJ=KIJS,KIJL
+            BTH(IJ)=BTH(IJ)*WAVNUM(IJ,M)*TPIINV*XK2CG(IJ,M)
+          ENDDO
+
+          DO IJ=KIJS,KIJL
+            D(IJ,K,M) = ZCOEF * (MAX(0._JWRB, BTH0(IJ)*TMP03-SSDSC4))**IPSAT + &
+ &                      ZCOEFM1 * (MAX(0._JWRB, BTH(IJ)*TMP03-SSDSC4))**IPSAT
+
+            ! cumulative term
+            SCUMUL(IJ,K,M) = 0._JWRB
+            IF(BTH0(IJ) > SDSBR)THEN
+              SCUMUL(IJ,K,M) = MAX(SQRT(BTH(IJ)) - EPSR, 0._JWRB)**2
+            ENDIF
+          ENDDO
+
         ENDDO
       ENDDO
 
@@ -184,42 +181,7 @@
 
         NANGD=NANG/2
 
-        DO M2=1,NFRE-NDIKCUMUL
-          DO IJ=KIJS,KIJL
-            IF (BTH0(IJ,M2) > SDSBR) THEN
-              TEMP1(IJ,M2)=1.0_JWRB
-            ELSE
-              TEMP1(IJ,M2)=0.0_JWRB
-            ENDIF
-          ENDDO
-        ENDDO
-        DO M2=1,NFRE-NDIKCUMUL
-          DO K2=1,NANG
-            DO IJ=KIJS,KIJL
-              SCUMUL(IJ,K2,M2)=TEMP1(IJ,M2)*(MAX(SQRT(BTH(IJ,K2,M2))-EPSR,0.0_JWRB))**2
-            ENDDO
-          ENDDO
-        ENDDO
-
         DO M=NDIKCUMUL+1,NFRE
-          DO K=1,NANG
-            DO IJ=KIJS,KIJL
-              RENEWALFREQ(IJ,K,M)=0.0_JWRB
-            ENDDO
-          ENDDO
-        ENDDO
-
-
-        DO M=NDIKCUMUL+1,NFRE
-
-          DO M2=1,M-NDIKCUMUL
-            DO KK=0,NANGD
-              DO IJ=KIJS,KIJL
-                WCUMUL(IJ,KK,M2)=CUMULW(INDEP(IJ),KK,M2,M)
-              ENDDO
-            ENDDO
-          ENDDO
-
           DO K=1,NANG
           ! Correction of saturation level for shallow-water kinematics
           ! Cumulative effect based on lambda   (breaking probability is
@@ -230,23 +192,23 @@
               IF ( KKD(K2) > NANGD) KKD(K2)=KKD(K2)-NANGD
             ENDDO
 
+            DO IJ=KIJS,KIJL
+              RENEWALFREQ(IJ)=0.0_JWRB
+            ENDDO
+
             DO M2=1,M-NDIKCUMUL
               DO K2=1,NANG
                 KK=KKD(K2)
                 DO IJ=KIJS,KIJL
                   ! Integrates over frequencies M2 and directions K2 to 
                   ! Integration is performed from M2=1 to a frequency lower than M: IK-NDIKCUMUL
-                  RENEWALFREQ(IJ,K,M)=RENEWALFREQ(IJ,K,M)+ WCUMUL(IJ,KK,M2)*SCUMUL(IJ,K2,M2)
+                  RENEWALFREQ(IJ)=RENEWALFREQ(IJ)+ CUMULW(INDEP(IJ),KK,M2,M)*SCUMUL(IJ,K2,M2)
                 ENDDO
               ENDDO
             ENDDO
-          ENDDO
-        ENDDO
 
-        DO M=NDIKCUMUL+1,NFRE
-          DO K=1,NANG
             DO IJ=KIJS,KIJL
-              D(IJ,K,M)= D(IJ,K,M) + RENEWALFREQ(IJ,K,M)
+              D(IJ,K,M)= D(IJ,K,M) + RENEWALFREQ(IJ)
             ENDDO
           ENDDO
         ENDDO
@@ -260,12 +222,9 @@
           FACTURB(IJ) = TMP01*RAORW(IJ)*UFRIC(IJ)*UFRIC(IJ)
         ENDDO
         DO M=1, NFRE
-          DO IJ=KIJS,KIJL
-            FACWTRB(IJ,M) = SIG(M)*WAVNUM(IJ,M)*FACTURB(IJ)
-          ENDDO
           DO K=1,NANG
             DO IJ=KIJS,KIJL
-              D(IJ,K,M)= D(IJ,K,M)- FACWTRB(IJ,M)*COSWDIF(IJ,K)
+              D(IJ,K,M)= D(IJ,K,M)- ZPIFR(M)*WAVNUM(IJ,M)*FACTURB(IJ)*COSWDIF(IJ,K)
             ENDDO
           ENDDO
         ENDDO
