01cc5808fc38e409d7a118d28845cbd2dfe0c96d
Josh Kousal <josh.kousal@ecmwf.int>
2023-07-31 14:56:36 +0000

    Introducing separate switches for 3 types of attenuation of waves in ice: scattering, bottom friction & viscous friction

---
+++ src/ecwam/sdice2.F90
@@ -0,0 +1,112 @@
+! (C) Copyright 1989- ECMWF.
+! 
+! This software is licensed under the terms of the Apache Licence Version 2.0
+! which can be obtained at http://www.apache.org/licenses/LICENSE-2.0.
+! In applying this licence, ECMWF does not waive the privileges and immunities
+! granted to it by virtue of its status as an intergovernmental organisation
+! nor does it submit to any jurisdiction.
+!
+
+      SUBROUTINE SDICE2 (KIJS, KIJL, FL1, FLD, SL,            &
+     &                   WAVNUM, CGROUP,                      &
+     &                   CICV)
+! ----------------------------------------------------------------------
+
+!**** *SDICE2* - COMPUTE SEA ICE WAVE ATTENUATION FACTORS DUE TO ICE FLOES
+!                BOTTOM FRICTION.
+
+
+!     JEAN BIDLOT       ECMWF ~2012
+
+
+!*    PURPOSE.
+!     --------
+
+!**   INTERFACE.
+!     ----------
+
+!       *CALL* *SDICE2 (KIJS, KIJL, FL1, FLD,SL,*
+!                       WAVNUM, CGROUP,
+!                       CICV)*
+!          *KIJS*   - INDEX OF FIRST GRIDPOINT
+!          *KIJL*   - INDEX OF LAST GRIDPOINT
+!          *FL1*    - SPECTRUM.
+!          *FLD*    - DIAGONAL MATRIX OF FUNCTIONAL DERIVATIVE
+!          *SL*     - TOTAL SOURCE FUNCTION ARRAY
+!          *WAVNUM* - WAVE NUMBER
+!          *CGROUP* - GROUP SPEED
+!          *CICV*   - SEA ICE COVER
+
+!     METHOD.
+!     -------
+
+!       SEE REFERENCES.
+
+!     EXTERNALS.
+!     ----------
+
+!       NONE.
+
+!     REFERENCE.
+!     ----------
+
+!     KOHOUT A., M. MEYLAN, D PLEW, 2011: ANNALS OF GLACIOLOGY, 2011. 
+!     M.J. Doble, J.-R. Bidlot / Ocean Modelling 70 (2013), 166-173
+
+! ----------------------------------------------------------------------
+
+      USE PARKIND_WAVE, ONLY : JWIM, JWRB, JWRU
+
+      USE YOWFRED  , ONLY : DFIM
+      USE YOWICE   , ONLY : CDICWA
+      USE YOWPARAM , ONLY : NANG    ,NFRE
+      USE YOWPCONS , ONLY : EPSMIN  
+
+      USE YOWTEST  , ONLY : IU06
+
+      USE YOMHOOK  , ONLY : LHOOK   ,DR_HOOK, JPHOOK
+
+! ----------------------------------------------------------------------
+
+      IMPLICIT NONE
+
+      INTEGER(KIND=JWIM), INTENT(IN) :: KIJS, KIJL
+
+      REAL(KIND=JWRB), DIMENSION(KIJS:KIJL,NANG,NFRE), INTENT(IN) :: FL1
+      REAL(KIND=JWRB), DIMENSION(KIJS:KIJL,NANG,NFRE), INTENT(INOUT) :: FLD, SL
+      REAL(KIND=JWRB), DIMENSION(KIJS:KIJL,NFRE), INTENT(IN) :: WAVNUM, CGROUP
+      REAL(KIND=JWRB), DIMENSION(KIJS:KIJL), INTENT(IN) :: CICV
+
+      REAL(KIND=JWRB), DIMENSION(NFRE)    :: XK2
+
+      INTEGER(KIND=JWIM) :: IJ, K, M
+      REAL(KIND=JWRB)    :: EWH
+      REAL(KIND=JWRB)    :: ALP              !! ALP=SPATIAL ATTENUATION RATE OF ENERGY
+      REAL(KIND=JWRB)    :: TEMP, EWH
+      
+      REAL(KIND=JPHOOK) :: ZHOOK_HANDLE
+
+
+! ----------------------------------------------------------------------
+
+      IF (LHOOK) CALL DR_HOOK('SDICE2',0,ZHOOK_HANDLE)
+
+      WRITE (IU06,*)'Ice attenuation due to bottom friction based on: '
+      WRITE (IU06,*)'  KOHOUT A., M. MEYLAN, D PLEW, 2011'
+
+      DO M = 1,NFRE
+         DO K = 1,NANG
+            DO IJ = KIJS,KIJL
+               EWH         = 4.0_JWRB*SQRT(MAX(EPSMIN,FL1(IJ,K,M)*DFIM(M)))
+               XK2(M)      = WAVNUM(IJ,M)**2
+               ALP         = CDICWA*XK2(M)*EWH
+               TEMP        = -CICV(IJ)*ALP*CGROUP(IJ,M)  
+               SL(IJ,K,M)  = SL(IJ,K,M)  + FL1(IJ,K,M)*TEMP
+               FLD(IJ,K,M) = FLD(IJ,K,M) + TEMP
+            END DO
+         END DO
+      END DO
+      
+      IF (LHOOK) CALL DR_HOOK('SDICE2',1,ZHOOK_HANDLE)
+
+      END SUBROUTINE SDICE2
---
+++ src/ecwam/sdice3.F90
@@ -0,0 +1,127 @@
+! (C) Copyright 1989- ECMWF.
+! 
+! This software is licensed under the terms of the Apache Licence Version 2.0
+! which can be obtained at http://www.apache.org/licenses/LICENSE-2.0.
+! In applying this licence, ECMWF does not waive the privileges and immunities
+! granted to it by virtue of its status as an intergovernmental organisation
+! nor does it submit to any jurisdiction.
+!
+
+      SUBROUTINE SDICE3 (KIJS, KIJL, FL1, FLD, SL,            &
+     &                   WAVNUM, CGROUP,                      &
+     &                   CICV,CITH)
+! ----------------------------------------------------------------------
+
+!**** *SDICE3* - COMPUTATION OF SEA ICE ATTENUATION DUE TO VISCOUS FRICTION
+
+
+!     LOTFI AOUF       METEO FRANCE 2023
+
+
+!*    PURPOSE.
+!     --------
+
+!**   INTERFACE.
+!     ----------
+
+!       *CALL* *SDICE3 (KIJS, KIJL, FL1, FLD,SL,
+!                       WAVNUM, CGROUP,                      
+!                       CICV,CITH)*
+!          *KIJS*   - INDEX OF FIRST GRIDPOINT
+!          *KIJL*   - INDEX OF LAST GRIDPOINT
+!          *FL1*    - SPECTRUM.
+!          *FLD*    - DIAGONAL MATRIX OF FUNCTIONAL DERIVATIVE
+!          *SL*     - TOTAL SOURCE FUNCTION ARRAY
+!          *WAVNUM* - WAVE NUMBER
+!          *CGROUP* - GROUP SPEED
+!          *CICV*   - SEA ICE COVER
+!          *CITH*   - SEA ICE THICKNESS
+
+!     METHOD.
+!     -------
+
+!       SEE REFERENCES.
+
+!     EXTERNALS.
+!     ----------
+
+!       NONE.
+
+!     REFERENCE.
+!     ----------
+
+!     Jie Yu * , W. Erick Rogers, David W. Wang, 2022 
+!     DOI:10.1016/j.coldregions.2022.103582
+
+! ----------------------------------------------------------------------
+
+      USE PARKIND_WAVE, ONLY : JWIM, JWRB, JWRU
+
+      USE YOWFRED  , ONLY : FR      
+      USE YOWPARAM , ONLY : NANG    ,NFRE
+      USE YOWPCONS , ONLY : G       ,ZPI
+
+      USE YOWTEST  , ONLY : IU06
+
+      USE YOMHOOK  , ONLY : LHOOK   ,DR_HOOK, JPHOOK
+
+! ----------------------------------------------------------------------
+
+      IMPLICIT NONE
+
+      INTEGER(KIND=JWIM), INTENT(IN) :: KIJS, KIJL
+
+      REAL(KIND=JWRB), DIMENSION(KIJS:KIJL,NANG,NFRE), INTENT(IN) :: FL1
+      REAL(KIND=JWRB), DIMENSION(KIJS:KIJL,NANG,NFRE), INTENT(INOUT) :: FLD, SL
+      REAL(KIND=JWRB), DIMENSION(KIJS:KIJL,NFRE), INTENT(IN) :: WAVNUM, CGROUP
+      REAL(KIND=JWRB), DIMENSION(KIJS:KIJL), INTENT(IN) :: CICV
+      REAL(KIND=JWRB), DIMENSION(KIJS:KIJL), INTENT(IN) :: CITH
+
+      INTEGER(KIND=JWIM) :: IMODEL                       !! DAMPING MODEL: 1=FIT TO TEMPELFJORD DATA, 2=Jie Yu 2022
+      INTEGER(KIND=JWIM) :: IJ, K, M
+      REAL(KIND=JWRB)    :: ALP              !! ALP=SPATIAL ATTENUATION RATE OF ENERGY
+      REAL(KIND=JWRB)    :: TEMP
+      REAL(KIND=JWRB)    :: CDICE
+      REAL(KIND=JWRB)    :: HICEMAX, HICEMIN
+      
+      REAL(KIND=JPHOOK) :: ZHOOK_HANDLE
+
+
+! ----------------------------------------------------------------------
+
+      IF (LHOOK) CALL DR_HOOK('SDICE3',0,ZHOOK_HANDLE)
+
+      IMODEL = 2
+!      IF (ITEST.GE.2) WRITE (IU06,*)'IMODEL =',IMODEL
+      HICEMAX=4.0_JWRB
+      HICEMIN=0.1_JWRB
+      WRITE (IU06,*)'Ice attenuation due to viscous friction based on: '
+      IF (IMODEL.EQ.1) THEN
+         WRITE (IU06,*)'  Best fit w Tempelfjorde obs from Lotfi Aouf'
+         CDICE=0.0656_JWRB
+      ELSE IF (IMODEL.EQ.2) THEN
+         WRITE (IU06,*)'  Jie Yu, W. Erik Rogers, David W. Wang 2022'
+         CDICE=0.1274_JWRB*( ZPI/SQRT(G) )**(4.5_JWRB)
+      END IF
+      
+      DO M = 1,NFRE
+         DO K = 1,NANG
+            DO IJ = KIJS,KIJL
+
+               IF (IMODEL.EQ.1) THEN
+                  ALP = CDICE*CITH(IJ)*WAVNUM(IJ,M)**2    
+               ELSE IF (IMODEL.EQ.2) THEN
+                  ALP = 2._JWRB*CDICE*(CITH(IJ)**(1.25_JWRB))*(FR(M)**(4.5_JWRB))
+               END IF
+
+               TEMP        = -CICV(IJ)*ALP*CGROUP(IJ,M)         
+               SL(IJ,K,M)  = SL(IJ,K,M)  + FL1(IJ,K,M)*TEMP
+               FLD(IJ,K,M) = FLD(IJ,K,M) + TEMP
+
+            END DO
+         END DO
+      END DO
+      
+      IF (LHOOK) CALL DR_HOOK('SDICE3',1,ZHOOK_HANDLE)
+
+      END SUBROUTINE SDICE3
