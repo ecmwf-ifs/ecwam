9f3337dc6f8034de16eb49f0806c7d2454531ccb
Jean Bidlot <jean.bidlot@ecmwf.int>
2024-05-15 13:33:20 +0000

    adapt since io server does not have access the wam NGRIB_VERSION

--- src/ecwam/wgrib_edition.F90
+++ src/ecwam/wgrib_edition.F90
@@ -12,14 +12,13 @@ INTEGER FUNCTION WGRIB_EDITION (IPARAMID)
 ! ----------------------------------------------------------------------
 
 !**** *WGRIB_EDITION* - FUNCTION TO DETERMINE WHETHER A WAVE PARAMETER WITH IPARAMID
-!                       CAN ONLY BE ENCODED IN GRIB 2.
-!                       ALL OTHER PARAMETERS WILL USE WHAT IS SPECFIED WITH NGRIB_VERSION
+!                       CAN ONLY BE ENCODED IN GRIB 2 (i.e. WGRIB_EDITION=2).
+!                       ALL OTHER PARAMETERS WGRIB_EDITION WILL RETURN 0
+!                       AND THE DEFAULT GRIB EDITION WILL NEED TO BE USED.
 
 ! ----------------------------------------------------------------------
 
 USE PARKIND_WAVE, ONLY : JWIM, JWRB, JWRU
-
-USE YOWGRIBHD, ONLY : NGRIB_VERSION
 
 ! ----------------------------------------------------------------------
 
@@ -29,17 +28,10 @@ INTEGER(KIND=JWIM), INTENT(IN) :: IPARAMID   ! grib parameter id (6 digit)
 
 ! ----------------------------------------------------------------------
 
-!!debile
-       write(0,*) 'wgrib_edition ',IPARAMID, NGRIB_VERSION
-
-
 IF ( IPARAMID >= 140131 .AND. IPARAMID <= 140134 ) THEN
   WGRIB_EDITION = 2
 ELSE
-  WGRIB_EDITION = NGRIB_VERSION
+  WGRIB_EDITION = 0 
 ENDIF
-
-!!debile
-       write(0,*) 'wgrib_edition ',WGRIB_EDITION
 
 END FUNCTION WGRIB_EDITION
