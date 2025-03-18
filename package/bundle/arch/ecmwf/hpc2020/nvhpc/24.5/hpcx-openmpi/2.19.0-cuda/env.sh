4ce94d2e732675b8fe10ee8c245c4a099cb286b5
Michael Staneker <michael.staneker@ecmwf.int>
2025-03-18 15:24:11 +0000

    NVHPC 24.5: add toolchain and env

---
+++ package/bundle/arch/ecmwf/hpc2020/nvhpc/24.5/hpcx-openmpi/2.19.0-cuda/env.sh
@@ -0,0 +1,51 @@
+# Source me to get the correct configure/build/run environment
+
+# Store tracing and disable (module is *way* too verbose)
+{ tracing_=${-//[^x]/}; set +x; } 2>/dev/null
+
+module_load() {
+  echo "+ module load $*"
+  module load $*
+}
+module_unload() {
+  echo "+ module unload $*"
+  module unload $*
+}
+module_purge() {
+  echo "+ module purge"
+  module purge
+}
+
+# Unload all modules to be certain
+[[ ${IFS_RUNTIME_ENV:-unset} == "unset" ]] && module_purge
+
+# Load modules
+module_load prgenv/nvidia
+module_load nvidia/24.5
+module_load hpcx-openmpi/2.19.0-cuda
+module_load openblas/0.3.26
+# Don't load these modules if env.sh is used as part of the IFS runtime environment - only the modules above are required
+if [[ ${IFS_RUNTIME_ENV:-unset} == "unset" ]]; then
+  module_load python3/3.11.8-01
+  module_load fftw/3.3.10
+  module_load netcdf4/4.9.2
+  module_load hdf5/1.14.3
+  module_load eigen/3.4.0
+  module_load cmake/3.28.3
+  module_load ninja/1.11.1
+  module_load fcm/2021.05.0
+  module_load aec/1.1.2
+fi
+
+# MKL envs
+export MKL_CBWR=AUTO,STRICT
+export MKL_NUM_THREADS=1
+export MKL_DYNAMIC=FALSE # Using capital letters
+export MKL_VERBOSE=${MKL_VERBOSE:-0} # if eq to 1, then each MKL func call as we go along will be output to ifs.out (stdout)
+export KMP_DETERMINISTIC_REDUCTION=true
+
+# Record the RPATH in the executable
+export LD_RUN_PATH=$LD_LIBRARY_PATH
+
+# Restore tracing to stored setting
+{ if [[ -n "$tracing_" ]]; then set -x; else set +x; fi } 2>/dev/null
