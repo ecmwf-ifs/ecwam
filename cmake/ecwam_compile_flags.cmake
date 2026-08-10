f5736b009a155e43a74a6769cd539aea4a897762
Ahmad Nawab <ahmad.nawab@ecmwf.int>
2026-08-10 17:43:40 +0200

    ecwam_compile_flags: set Kieee for nvhpc builds

--- cmake/ecwam_compile_flags.cmake
+++ cmake/ecwam_compile_flags.cmake
@@ -47,6 +47,7 @@ elseif(CMAKE_Fortran_COMPILER_ID MATCHES "PGI|NVHPC")
   set(vectorization_flags "-O3 -fast")
   string(REPLACE "-O2" "" ${PNAME}_Fortran_FLAGS_BIT ${${PNAME}_Fortran_FLAGS_BIT})
   set(checkbounds_flags   "-Mbounds")
+  set(fpmodel_flags       "-Kieee")
 
 elseif(CMAKE_Fortran_COMPILER_ID MATCHES "Flang")
   set(autopromote_flags   "-fdefault-real-8")
